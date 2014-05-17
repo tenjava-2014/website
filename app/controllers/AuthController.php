8fe56e5d4d1df7b61858a1ad5b961d76505cb886
lol768 <lol768@lol768.com>
2014-05-17 22:14:36 +0100

    Add login with GH

---
+++ app/controllers/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+class AuthController {
+    public function loginWithGitHub() {
+        // get data from input
+        $code = Input::get('code');
+
+        $githubService = OAuth::consumer('GitHub');
+
+        if (!empty($code)) {
+
+            // This was a callback request from github, get the token
+            $token = $githubService->requestAccessToken($code);
+
+            // Send a request with it
+            $result = json_decode($githubService->request('user'), true);
+
+            $message = 'Your unique GitHub user id is: ' . $result['id'] . ' and your name is ' . $result['login'];
+            echo $message . "<br/>";
+
+            //Var_dump
+            //display whole array().
+            dd($result);
+
+        } // if not ask for permission first
+        else {
+            $url = $githubService->getAuthorizationUri();
+
+            // return to github login url
+            return Redirect::to((string)$url);
+        }
+    }
+
+} 
