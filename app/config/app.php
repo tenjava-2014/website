4c9b7ed73653871a004563a25bd3f62eee175e94
lol768 <lol768@lol768.com>
2014-05-17 22:05:07 +0100

    Add in oauth config and change app config accordingly

--- app/config/app.php
+++ app/config/app.php
@@ -119,6 +119,7 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
+        'Artdarek\OAuth\OAuthServiceProvider'
 
 	),
 
@@ -185,6 +186,7 @@ return array(
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
+        'OAuth'           => 'Artdarek\OAuth\Facade\OAuth',
 
 	),
 
---
+++ app/config/oauth-4-laravel.php
@@ -0,0 +1,31 @@
+<?php
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | oAuth Config
+    |--------------------------------------------------------------------------
+    */
+
+    /**
+     * Storage
+     */
+    'storage' => 'Session',
+
+    /**
+     * Consumers
+     */
+    'consumers' => array(
+
+        /**
+         * GitHub
+         */
+        'GitHub' => array(
+            'client_id'     => '',
+            'client_secret' => '',
+            'scope'         => array('user'),
+        ),
+
+    )
+
+);
