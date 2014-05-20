603c3afacd3f9fa4506b7745431dce67b66c5846
lol768 <lol768@lol768.com>
2014-05-20 15:09:06 +0100

    Let's try using the static function to get a Carbon object

--- app/controllers/PointsController.php
+++ app/controllers/PointsController.php
@@ -1,13 +1,14 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Filesystem\Filesystem;
 
 class PointsController extends BaseController {
     public function showLeaderboard() {
         $filesystem = new Filesystem();
         $pointsData = json_decode($filesystem->get(public_path("assets/data.json")));
-        $carbonLast = new \Carbon\Carbon((int) ($pointsData->last_update / 1000));
-        $carbonNext = new \Carbon\Carbon((int) ($pointsData->next_update / 1000));
+        $carbonLast = Carbon::createFromTimestamp($pointsData->last_update);
+        $carbonNext = Carbon::createFromTimestamp($pointsData->next_update);
         return View::make("points")->with(array("data" => $pointsData, "next" => $carbonNext, "last" => $carbonLast));
     }
 } 
