5642d6296f36d5384c348806c16909ab2e32140b
lol768 <lol768@lol768.com>
2014-05-20 15:03:36 +0100

    Carbon likes ints

--- app/controllers/PointsController.php
+++ app/controllers/PointsController.php
@@ -6,8 +6,8 @@ class PointsController extends BaseController {
     public function showLeaderboard() {
         $filesystem = new Filesystem();
         $pointsData = json_decode($filesystem->get(public_path("assets/data.json")));
-        $carbonLast = new \Carbon\Carbon($pointsData->last_update / 1000);
-        $carbonNext = new \Carbon\Carbon($pointsData->next_update / 1000);
+        $carbonLast = new \Carbon\Carbon((int) $pointsData->last_update / 1000);
+        $carbonNext = new \Carbon\Carbon((int) $pointsData->next_update / 1000);
         return View::make("points")->with(array("data" => $pointsData, "next" => $carbonNext, "last" => $carbonLast));
     }
 } 
