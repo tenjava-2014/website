ad2bf777170cbec2c1c522088eb3b4bf9ac93765
lol768 <lol768@lol768.com>
2014-05-29 22:42:13 +0100

    Fix relative paths

--- app/sass/config.rb
+++ app/sass/config.rb
@@ -2,10 +2,10 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "../public/assets/css/"
+css_dir = "../../public/assets/css/"
 sass_dir = "."
-images_dir = "../public/assets/img/"
-javascripts_dir = "../public/assets/js/"
+images_dir = "../../public/assets/img/"
+javascripts_dir = "../../public/assets/js/"
 
 # You can select your preferred output style here (can be overridden via the command line):
 output_style = :compressed
