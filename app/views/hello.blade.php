c96952d4821a5197e2319bc4e6407cb41b3fd699
Justin <justin@battleplugins.com>
2014-05-29 16:09:29 -0400

    Convert HTML template to blade

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -1,59 +1,97 @@
-@extends('layout')
-
+@extends('layouts.master')
 @section('content')
-<div class="three centered columns">
-    <p><a href="/"><img id="logo" src="/assets/img/drawing_1.svg"></a></p>
+<div id="header">
+	<div class="grid-container">
+		<div class="grid-100 text-center">
+			Occaecat illustriora ea graviterque
+		</div>
+	</div>
 </div>
-<div class="twelve columns" id="intro">
-    <p><strong>ten.java</strong> is an unofficial, bi-annual Bukkit plugin development contest. Created in early
-        November by nkrecklow with the first ever contest taking place on the 7th December 2013, ten.java is a
-        ten hour competition to create an original plugin based on a theme. Plugins are judged by a group of
-        volunteers and we use CurseForge points to award prizes to the winning developers. Last year we had just
-        under 90 registered participants.</p>
+<div class="content-back">
+	<div class="grid-container">
+		<div class="grid-60">
+			<p><strong>ten.java</strong> is an unofficial, bi-annual Bukkit plugin development contest. Created in early
+				November by nkrecklow with the first ever contest taking place on the 7th December 2013, ten.java is a
+				ten hour competition to create an original plugin based on a theme. Plugins are judged by a group of
+				volunteers and we use CurseForge points to award prizes to the winning developers. Last year we had just
+				under 90 registered participants.</p>
 
-    <p>This time we're hoping to see even more people participating. We've set a provisional date of <strong>July
-            12th</strong> for this year's first event. If you're interested in getting involved as a
-        participant, please signup below. The only thing you need is a GitHub account (to commit your code) and
-        a BukkitDev account (to receive your points if you win). You can also apply to be a judge below.</p>
+			<p>This time we're hoping to see even more people participating. We've set a provisional date of <strong>July
+					12th</strong> for this year's first event. If you're interested in getting involved as a
+				participant, please signup below. The only thing you need is a GitHub account (to commit your code) and
+				a BukkitDev account (to receive your points if you win). You can also apply to be a judge below.</p>
 
-    <p>When you use the buttons below, you'll be redirected to GitHub to authorise the application. We ask GitHub for an
-        email so that we can contact you if you win (this info will not be shared and will be used for the sole purpose
-        of communications directly related to the contest). We also use the API to retrieve your GitHub username. This
-        username is used to create a repository on the tenjava github account and then added to the newly-created repo
-        with full
-        access.
-        @if (!$noEmail)
-            If you're unwilling to provide an email at any stage, please click <a href="/no-email">here</a> and then signup
-            normally.
-        @else
-            You're currently opted out of email sharing. If you wish to opt back in, please click <a
-                href="/no-email?undo=1">here</a> and then signup
-            normally.
-        @endif
-    </p>
-
-    <div class="six centered columns" id="buttons">
-        <div class="row">
-            <div class="medium metro rounded btn primary icon-left entypo icon-trophy">
-                <a href="/register">Register as participant</a>
-            </div>
-            <div class="medium metro rounded btn primary icon-left entypo icon-users">
-                <a href="/judge">Apply as judge</a>
-            </div>
-        </div>
-        <div class="row">
-            <div class="medium metro rounded btn default icon-left entypo icon-twitter">
-                <a href="https://twitter.com/tenjava">Twitter</a>
-            </div> <div class="medium metro rounded btn default icon-left entypo icon-github">
-<a href="https://github.com/tenjava">GitHub</a>
+			<p>When you use the signup buttons, you'll be redirected to GitHub to authorise the application. We ask GitHub for an
+				email so that we can contact you if you win (this info will not be shared and will be used for the sole purpose
+				of communications directly related to the contest). We also use the API to retrieve your GitHub username. This
+				username is used to create a repository on the tenjava github account and then added to the newly-created repo
+				with full
+				access.
+				If you're unwilling to provide an email at any stage, please click <a href="/no-email">here</a> and then signup
+				normally.
+			</p>
+		</div>
+		<div class="grid-30 mobile-grid-100 tablet-grid-100 pull-right text-center">
+			<p>
+				<a href="#" class="button button-large button-block button-flat-action">Register as Participant</a>
+				<span class="text-light">There are currently 123 participants</span>
+			</p>
+			<p>
+				<a href="#" class="button button-block button-flat-primary">Apply to Become a Judge</a>
+				<span class="text-light">There are currently 10 judges</span>
+			</p>
+			<p>
+				<a href="#" class="button button-block button-flat-highlight">Make a Donation</a>
+				<span class="text-light">We've raised 10000 points! That's a whopping $500!</span>
+			</p>
+		</div>
+	</div>
 </div>
-            <div class="medium metro rounded btn default icon-left entypo icon-heart">
-                <a href="/points">Points leaderboard</a>
-            </div>
-        </div>
-    </div>
-    <div style="text-align: center" class="ten centered columns">
-        <h3 style="color: #aaa">Total number of signups: {{{ $signups }}}</h3>
-    </div>
+<div class="grid-container">
+	<div class="grid-20 mobile-grid-20 tablet-grid-20 twitch">
+		<div class="twitch-snapshot">
+			<img src="http://static-cdn.jtvnw.net/previews-ttv/live_user_nightblue3-600x400.jpg" width="100%" />
+		</div>
+		<div class="twitch-footer">
+			lol768's stream
+		</div>
+	</div>
+	<div class="grid-20 mobile-grid-20 tablet-grid-20 twitch">
+		<div class="twitch-snapshot">
+			<img src="http://static-cdn.jtvnw.net/previews-ttv/live_user_tsm_theoddone-600x400.jpg" width="100%" />
+		</div>
+		<div class="twitch-footer">
+			lol768's stream
+		</div>
+	</div>
+	<div class="grid-20 mobile-grid-20 tablet-grid-20 twitch">
+		<div class="twitch-snapshot">
+			<img src="http://static-cdn.jtvnw.net/previews-ttv/live_user_vgbootcamp-600x400.jpg" width="100%" />
+		</div>
+		<div class="twitch-footer">
+			lol768's stream
+		</div>
+	</div>
+	<div class="grid-20 mobile-grid-20 tablet-grid-20 twitch">
+		<div class="twitch-snapshot">
+			<img src="http://static-cdn.jtvnw.net/previews-ttv/live_user_riotgames-600x400.jpg" width="100%" />
+		</div>
+		<div class="twitch-footer">
+			lol768's stream
+		</div>
+	</div>
+	<div class="grid-20 mobile-grid-20 tablet-grid-20 twitch">
+		<div class="twitch-snapshot">
+			<img src="http://static-cdn.jtvnw.net/previews-ttv/live_user_nightblue3-600x400.jpg" width="100%" />
+		</div>
+		<div class="twitch-footer">
+			lol768's stream
+		</div>
+	</div>
+	<!--
+<div class="grid-100 text-right">
+<small><a href="#">View all streamers</a></small>
+</div>
+-->
 </div>
 @stop
---
+++ app/views/layouts/master.blade.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<title>ten.java 2014</title>
+	<link rel="icon" type="image/png" href="/assets/img/favicon.ico" />
+	<link href="/assets/css/grid.css" rel="stylesheet" />
+	<link href="/assets/css/styles.css" rel="stylesheet" />
+	<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.1.0/css/font-awesome.min.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1" />
+</head>
+<body>
+<div id="wrapper">
+	<nav>
+		<div class="grid-container">
+			<div class="grid-30 brand">
+				<a href="#"><img src="https://cdn.mediacru.sh/PghQ0cj1j2YI.svg" height="50px" border="0" /></a>
+			</div>
+			<div class="grid-70">
+				<ul class="nav-links">
+					<li class="active"><a href="#">Home</a></li>
+					<li><a href="#">Signup</a></li>
+					<li><a href="#">Points</a></li>
+					<li><a href="#">Judges</a></li>
+					<li><a href="#">About</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<div id="point-ticker">
+		<div class="grid-container">
+			<div class="grid-20 tablet-grid-20">Latest Donation: <span>alkarinv (2500)</span></div>
+			<div class="grid-20 tablet-grid-20">Top Donation: <span>alkarinv (2500)</span></div>
+			<div class="grid-20 tablet-grid-20">Total Points: <span>9348 ($433.56)</span></div>
+			<div class="grid-20 tablet-grid-20">Last Signup: <span>jkcclemens</span></div>
+			<div class="grid-20 tablet-grid-20">Total Signups: <span>122 developers</span></div>
+		</div>
+	</div>
+	@yield('content')
+	<div class="push"></div>
+</div>
+<footer>
+	<div class="grid-container">
+		<div class="grid-80">
+			<p>&copy; 2014 ten.java - All Rights Reserved.<br />
+				ten.java (competition and website) is not affiliated with Bukkit, Curse, or Mojang in any way.</p>
+		</div>
+		<div class="grid-20">
+			<ul class="social-media pull-right">
+				<li><a href="mailto:contact@tenjava.com"><i class="fa fa-2x fa-envelope-square"></i></a></li>
+				<li><a  title="" target="_blank" href="https://github.com/tenjava"><i class="fa fa-2x fa-github-square"></i></a></li>
+				<li><a  target="_blank" href="https://twitter.com/tenjava"><i class="fa fa-2x fa-twitter-square"></i></a></li>
+			</ul>
+		</div>
+	</div>
+</footer>
+</body>
+</html>
