c3c3876d7ae068a421cc662698cea2861730029b
lol768 <lol768@infinitenight.net>
2014-05-30 03:31:30 +0100

    Add in route groups

--- app/routes.php
+++ app/routes.php
@@ -11,17 +11,36 @@
 |
 */
 
-Route::get('/', "HomeController@index");
-Route::get('/register/participant', "AppController@applyParticipant");
-Route::get('/register/judge', "AppController@applyJudge");
-Route::get('/oauth/confirm', 'AuthController@loginWithGitHub');
-Route::post('/apply', 'AppController@processApplication');
-Route::get('/list', 'AppController@listApps');
-Route::get('/decline/{id}', 'AppController@declineJudgeApp');
+/* NO AUTH REQUIRED */
+Route::group(array(), function () {
+    Route::get('/', "HomeController@index");
+    Route::get('/points', 'PointsController@showLeaderboard');
+    Route::get('/oauth/confirm', 'AuthController@loginWithGitHub');
+    Route::get('/no-email', 'AppController@noEmail');
+});
 
-Route::get('/no-email', 'AppController@noEmail');
-Route::get('/points', 'PointsController@showLeaderboard');
+/* API - NO AUTH REQUIRED */
+Route::group(array(), function () {
+    Route::get('/api/participants', 'ApiController@getParticipants');
+    Route::get('/api/points', 'ApiController@getPoints');
+    Route::get('/api/session', 'ApiController@getSessionData');
+});
 
-Route::get('/api/participants', 'ApiController@getParticipants');
-Route::get('/api/points', 'ApiController@getPoints');
-Route::get('/api/session', 'ApiController@getSessionData');
+/* LOGGED IN USERS ONLY */
+Route::group(array('before' => 'AuthenticationFilter'), function () {
+    Route::get('/register/participant', "AppController@applyParticipant");
+    Route::get('/register/judge', "AppController@applyJudge");
+    Route::post('/apply', 'AppController@processApplication');
+});
+
+/* JUDGES ONLY */
+Route::group(array('before' => 'StaffFilter'), function () {
+    Route::get('/list', 'AppController@listApps');
+});
+
+/* ORGANIZERS ONLY */
+Route::group(array('before' => 'StaffFilter'), function () {
+    Route::get('/decline/{id}', 'AppController@declineJudgeApp');
+});
+
+
