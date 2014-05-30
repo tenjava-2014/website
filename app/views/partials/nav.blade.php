8c85e9c5488b7d2601cbd32da8b9fd27d4d1954b
lol768 <lol768@infinitenight.net>
2014-05-30 02:59:57 +0100

    Make logo clickable

--- app/views/partials/nav.blade.php
+++ app/views/partials/nav.blade.php
@@ -1,7 +1,7 @@
 <nav>
 	<div class="grid-container">
 		<div class="grid-30 tablet-grid-100 brand">
-			<a href="#"><img src="https://cdn.mediacru.sh/PghQ0cj1j2YI.svg" height="50px" border="0" /></a>
+			<a href="/"><img src="https://cdn.mediacru.sh/PghQ0cj1j2YI.svg" height="50px" border="0" /></a>
 		</div>
 		<div class="grid-70 tablet-grid-100">
 			<ul class="nav-links">
