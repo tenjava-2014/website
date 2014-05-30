eab2d95da96c56cebdba0df513066928555f712a
Justin <justin@battleplugins.com>
2014-05-30 14:49:29 -0400

    Remove header and substitute own class

--- app/views/partials/twitter.blade.php
+++ app/views/partials/twitter.blade.php
@@ -4,8 +4,8 @@
 	</div>
 	@foreach ($tweets as $tweet)
 	<div class="grid-20 grid-parent">
-		<div class="content-box">
-			<div class="grid-100"><h5>{{ Twitter::ago($tweet['created_at']) }}</h5></div>
+		<div class="content-back">
+			<div class="grid-100 time">{{ Twitter::ago($tweet['created_at']) }}</div>
 			<div class="grid-100">{{ Twitter::linkify($tweet['text']) }}</div>
 		</div>
 	</div>
