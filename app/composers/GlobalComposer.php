90cfbb555e0f722fcb167c0eb4e5ea34ac044885
Justin <justin@battleplugins.com>
2014-05-30 10:25:30 -0400

    There should be a config

--- app/composers/GlobalComposer.php
+++ app/composers/GlobalComposer.php
@@ -9,7 +9,7 @@ class GlobalComposer {
         $fs = new Filesystem();
         $appsCount = Application::where("judge", false)->count();
         $latestAppName = Application::where("judge", false)->orderBy("id", "desc")->pluck("gh_username");
-	    $twitter = new Twitter;
+	    $twitter = new Twitter();
 	    $twitter = $twitter->getUserTimeline(array('screen_name' => 'tenjava', 'count' => 1, 'format' => 'array'));
 
         $view->with('pointsData', json_decode($fs->get(public_path("assets/data.json"))));
