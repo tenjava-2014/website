ca37a335d4e0b81ebdfdb3a1c105624f22529e2f
lol768 <lol768@lol768.com>
2014-05-30 18:08:53 +0100

    Maybe it's an array?

--- app/views/partials/twitter.blade.php
+++ app/views/partials/twitter.blade.php
@@ -2,7 +2,7 @@
 	<div class="grid-100">
         <ul>
             @foreach ($tweets as $tweet)
-                <li>{{{ $tweet->text }}}</li>
+                <li>{{{ $tweet['text'] }}}</li>
             @endforeach
             </ul>
 	</div>
