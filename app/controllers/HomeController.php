127a83cff86e9b28bfd6b2ec3b4c53dfec3ce6af
lol768 <lol768@infinitenight.net>
2014-05-30 01:58:25 +0100

    Unneeded variable

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -16,8 +16,8 @@ class HomeController extends BaseController {
     */
 
     public function showWelcome() {
-	    parent::setActive("Home");
-        return View::make('hello')->with(array("noEmail" => Session::has("no-email"), "signups" => $apps));
+        parent::setActive("Home");
+        return View::make('hello')->with(array("noEmail" => Session::has("no-email")));
     }
 
 }
