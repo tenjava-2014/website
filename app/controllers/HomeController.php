d20d808edc44588eb1a69966a3dda9651ed6db37
lol768 <lol768@infinitenight.net>
2014-05-30 02:44:22 +0100

    Refactor view locations

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -2,23 +2,10 @@
 
 class HomeController extends BaseController {
 
-    /*
-    |--------------------------------------------------------------------------
-    | Default Home Controller
-    |--------------------------------------------------------------------------
-    |
-    | You may wish to use controllers instead of, or in addition to, Closure
-    | based routes. That's great! Here is an example controller method to
-    | get you started. To route to this controller, just add the route:
-    |
-    |	Route::get('/', 'HomeController@showWelcome');
-    |
-    */
-
-    public function showWelcome() {
+    public function index() {
         parent::setActive("Home");
         $this->setPageTitle("Home");
-        return View::make('hello')->with(array("noEmail" => Session::has("no-email")));
+        return View::make('pages.static.home');
     }
 
 }
--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/', "HomeController@showWelcome");
+Route::get('/', "HomeController@index");
 Route::get('/register', "AppController@applyParticipant");
 Route::get('/judge', "AppController@applyJudge");
 Route::get('/oauth/confirm', 'AuthController@loginWithGitHub');
