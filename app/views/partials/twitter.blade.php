a6f23b3cd7a00ea0e9fbce7f218d7a983cc5e955
Justin <justin@battleplugins.com>
2014-05-30 17:28:00 -0400

    Change twitter grid

--- app/views/partials/twitter.blade.php
+++ app/views/partials/twitter.blade.php
@@ -3,7 +3,7 @@
 		<h3>Tweets from {{ Twitter::linkify('@tenjava') }}</h3>
 	</div>
 	@foreach ($tweets as $tweet)
-	<div class="grid-20 grid-parent">
+	<div class="grid-20 tablet-grid-100 grid-parent">
 		<div class="grid-100 time"><span title="{{{ $tweet['created_at'] }}}">{{{ Twitter::ago($tweet['created_at']) }}}</span></div>
 		<div class="grid-100">
 			<div class="content-back">
