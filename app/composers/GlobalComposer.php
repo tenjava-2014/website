64448cc23db685189bcd73ee539b7fd2e8a24027
Justin <justin@battleplugins.com>
2014-05-30 09:38:40 -0400

    Instantiate twitter

--- app/composers/GlobalComposer.php
+++ app/composers/GlobalComposer.php
@@ -9,10 +9,12 @@ class GlobalComposer {
         $fs = new Filesystem();
         $appsCount = Application::where("judge", false)->count();
         $latestAppName = Application::where("judge", false)->orderBy("id", "desc")->pluck("gh_username");
+	    $twitter = new Twitter;
+	    $twitter = $twitter->getUserTimeline(array('screen_name' => 'tenjava', 'count' => 1, 'format' => 'array'));
 
         $view->with('pointsData', json_decode($fs->get(public_path("assets/data.json"))));
         $view->with('appsData', (object) ["count" => $appsCount, "latestUsername" => $latestAppName]);
-	    $view->with('lastTweet', Twitter::getUserTimeline(array('screen_name' => 'tenjava', 'count' => 1, 'format' => 'array')));
+	    $view->with('lastTweet', $twitter);
     }
 
 }
