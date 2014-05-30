d27046819206c87298abcfe6d57434ff242d860f
Justin <justin@battleplugins.com>
2014-05-30 08:51:54 -0400

    Vertical align icon

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -36,7 +36,7 @@
 				<li><a href="mailto:contact@tenjava.com"><i class="fa fa-2x fa-envelope-square"></i></a></li>
 				<li><a title="" target="_blank" href="https://github.com/tenjava"><i class="fa fa-2x fa-github-square"></i></a></li>
 				<li><a target="_blank" href="https://twitter.com/tenjava"><i class="fa fa-2x fa-twitter-square"></i></a></li>
-				<li><a target="_blank" href="http://forums.bukkit.org/threads/269253/"><span class="fa-stack"><i class="fa fa-square fa-stack-2x"></i><i class="fa fa-comment fa-stack-1x black"></i></span></a></li>
+				<li><a target="_blank" href="http://forums.bukkit.org/threads/269253/"><span class="fa-stack bottom"><i class="fa fa-square fa-stack-2x"></i><i class="fa fa-comment fa-stack-1x black"></i></span></a></li>
 			</ul>
 		</div>
 	</div>
