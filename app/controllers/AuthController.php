557296ea3c8ea50c41bcfc195f2a11c879aa00f6
lol768 <lol768@lol768.com>
2014-05-30 18:14:36 +0100

    Remove die statement. Things seem to work now.

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -33,7 +33,6 @@ class AuthController extends BaseController {
             }
             $githubUsername = $result['login'];
             Session::put("application_data", array("username" => $githubUsername, "emails" => $emails));
-            die(Session::get("previous"));
             return Redirect::to(Session::get("previous"));
 
         } // if not ask for permission first
