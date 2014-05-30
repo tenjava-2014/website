a91243a8c76253ce4e8b15725182bdf4d54ad712
Justin <justin@battleplugins.com>
2014-05-30 14:44:34 -0400

    Put title in header

--- app/views/partials/twitter.blade.php
+++ app/views/partials/twitter.blade.php
@@ -1,13 +1,11 @@
 <div class="grid-container">
 	<div class="grid-100">
-		Tweets from {{ Twitter::linkify('@tenjava') }}
+		<h3>Tweets from {{ Twitter::linkify('@tenjava') }}</h3>
 	</div>
-	<div class="grid-100">
-		@foreach ($tweets as $tweet)
-		<div class="grid-20 grid-parent">
-			<div class="grid-100">{{ Twitter::ago($tweet['created_at']) }}</div>
-			<div class="grid-100">{{ Twitter::linkify($tweet['text']) }}</div>
-		</div>
-		@endforeach
+	@foreach ($tweets as $tweet)
+	<div class="grid-20 grid-parent">
+		<div class="grid-100">{{ Twitter::ago($tweet['created_at']) }}</div>
+		<div class="grid-100">{{ Twitter::linkify($tweet['text']) }}</div>
 	</div>
+	@endforeach
 </div>
