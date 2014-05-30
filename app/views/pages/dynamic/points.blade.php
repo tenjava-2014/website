e1a14efaf488ecd4127756891a43071bf8a47aef
Aero <aero@kpvp.in>
2014-05-31 00:19:23 +0100

    Adding a total donator count to the points page

--- app/views/pages/dynamic/points.blade.php
+++ app/views/pages/dynamic/points.blade.php
@@ -4,6 +4,8 @@
     <div class="grid-container">
         <div class="grid-100">
             <p>Points!</p>
+
+            <p>So far, {{{ $totalCount }}} people have donated points.</p>
         </div>
     </div>
 </div>
