bcae103f231e24e21411b322f868722579ffd4c5
lol768 <lol768@infinitenight.net>
2014-05-30 04:13:00 +0100

    Rename function accordingly

--- app/controllers/basic/SignupController.php
+++ app/controllers/basic/SignupController.php
@@ -3,7 +3,7 @@
 
 class SignupController extends BaseController {
 
-    public function showAbout() {
+    public function showSignup() {
         $this->setPageTitle("Signup");
         $this->setActive("signup");
         return Response::view('pages.static.signup', array());
