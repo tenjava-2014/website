f79d93a4d3f4c154864fab0a9fac4433138845af
Justin <justin@battleplugins.com>
2014-05-30 14:46:53 -0400

    Some more styles

--- app/views/partials/twitter.blade.php
+++ app/views/partials/twitter.blade.php
@@ -4,8 +4,10 @@
 	</div>
 	@foreach ($tweets as $tweet)
 	<div class="grid-20 grid-parent">
-		<div class="grid-100">{{ Twitter::ago($tweet['created_at']) }}</div>
-		<div class="grid-100">{{ Twitter::linkify($tweet['text']) }}</div>
+		<div class="content-box">
+			<div class="grid-100"><h5>{{ Twitter::ago($tweet['created_at']) }}</h5></div>
+			<div class="grid-100">{{ Twitter::linkify($tweet['text']) }}</div>
+		</div>
 	</div>
 	@endforeach
 </div>
