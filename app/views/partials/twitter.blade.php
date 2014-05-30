e4c98a33b5c2f1292ce59ba1ec3e51ecefd1bae3
Justin <justin@battleplugins.com>
2014-05-30 14:50:28 -0400

    Change nesting

--- app/views/partials/twitter.blade.php
+++ app/views/partials/twitter.blade.php
@@ -4,9 +4,11 @@
 	</div>
 	@foreach ($tweets as $tweet)
 	<div class="grid-20 grid-parent">
-		<div class="content-back">
-			<div class="grid-100 time">{{ Twitter::ago($tweet['created_at']) }}</div>
-			<div class="grid-100">{{ Twitter::linkify($tweet['text']) }}</div>
+		<div class="grid-100 time">{{ Twitter::ago($tweet['created_at']) }}</div>
+		<div class="grid-100">
+			<div class="content-back">
+				{{ Twitter::linkify($tweet['text']) }}
+			</div>
 		</div>
 	</div>
 	@endforeach
