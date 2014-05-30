1ca1d7bb30755effe4ce62a940e6f7ed6b4092c5
lol768 <lol768@lol768.com>
2014-05-30 20:09:13 +0100

    Add tooltips to time infos

--- app/views/partials/twitter.blade.php
+++ app/views/partials/twitter.blade.php
@@ -4,7 +4,7 @@
 	</div>
 	@foreach ($tweets as $tweet)
 	<div class="grid-20 grid-parent">
-		<div class="grid-100 time">{{ Twitter::ago($tweet['created_at']) }}</div>
+		<div class="grid-100 time"><span title="{{{ $tweet['created_at'] }}}">{{{ Twitter::ago($tweet['created_at']) }}}</span></div>
 		<div class="grid-100">
 			<div class="content-back">
 				{{ Twitter::linkify($tweet['text']) }}
