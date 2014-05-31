6bbc6f47f698a39c40f0057deb1824f00d64668a
Justin <justin@battleplugins.com>
2014-05-31 16:16:50 -0400

    Increase size of instructions

--- app/views/pages/dynamic/points.blade.php
+++ app/views/pages/dynamic/points.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div class="content-back">
 	<div class="grid-container">
-		<div class="grid-40">
+		<div class="grid-45">
 			<p>Prizes for the winners of the contest are in the form of CurseForge points which are given out to all developers who sign up to the scheme (applies to BukkitDev, CurseForge and other sites). Developers with more popular projects will receive more points. We rely on point donations to form the contest prize. The more points given, the more we can give out to the winning developers.</p>
 			<h4>So far, we've raised {{ number_format($data->points) }} (${{ number_format(round($data->points / 20, 2)) }}) points from {{{ $totalCount }}} people. That's {{ $goal }}% more than last year's amount!</h4>
 		</div>
@@ -11,6 +11,9 @@
 				the 'tenjava' user.
 			</p>
 			<img src="/assets/img/points.png" />
+			<p>
+				<small>Example points transfer.</small>
+			</p>
 		</div>
 	</div>
 </div>
