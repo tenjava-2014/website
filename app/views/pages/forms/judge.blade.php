e3e18dbe8b17751d55758a2a59a9dcd4315d76e6
lol768 <lol768@lol768.com>
2014-05-31 23:09:58 +0100

    Add alerts

--- app/views/pages/forms/judge.blade.php
+++ app/views/pages/forms/judge.blade.php
@@ -3,7 +3,27 @@
 <div class="content-back">
     <div class="grid-container">
         <div class="grid-100">
-            <p>Insert judge form!</p>
+            <h2>Apply to be a judge</h2>
+            <div class="alert basic success">
+                <button class="dismiss">×</button>
+                <p><b>Done:</b> Process completed successfully</p>
+            </div>
+            <p>Please fill out the following fields to register for the 2014 ten.java contest.</p>
+            <form class="form" action="/apply/participant">
+                <div class="control-group">
+                    <label for="bukkitdev">BukkitDev username</label>
+                    <div class="control">
+                        <input id="bukkitdev" type="text" value="{{{ $username }}}" name="dbo">
+                    </div>
+                </div>
+                <div class="control-group">
+                    <label for="twitch">twitch.tv username <span class="optional">(optional)</span></label>
+                    <div class="control">
+                        <input id="twitch" type="text" value="{{{ $username }}}" name="twitch">
+                    </div>
+                </div>
+                <input type="submit" value="Sign up" class="button button-block button-flat-primary">
+            </form>
         </div>
     </div>
 </div>
