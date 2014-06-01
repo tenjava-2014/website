2c16ab1198461f9c0227977ff7dbc0b1bf660089
lol768 <lol768@lol768.com>
2014-06-01 19:37:32 +0100

    Fix nav overlaps

--- app/views/partials/nav.blade.php
+++ app/views/partials/nav.blade.php
@@ -1,6 +1,6 @@
 <nav>
 	<div class="grid-container">
-		<div class="grid-30 tablet-grid-100 brand">
+		<div class="grid-15 tablet-grid-100 brand">
 			<a href="/"><img src="https://cdn.mediacru.sh/PghQ0cj1j2YI.svg" height="50px" border="0" /></a>
             <div class="hide-on-desktop">
                 <a href="#" id="nav-toggle">
@@ -8,7 +8,7 @@
                 </a>
             </div>
 		</div>
-		<div class="grid-70 tablet-grid-100 hide-on-mobile" id="nav-container">
+		<div class="grid-85 tablet-grid-100 hide-on-mobile" id="nav-container">
 			<ul class="nav-links">
 				@foreach($nav['primary'] as $navItem)
 				    <li @if($navItem->isActive()) class="active" @endif><a href="{{ $navItem->getUrl() }}">{{ $navItem->getTitle() }}</a></li>
