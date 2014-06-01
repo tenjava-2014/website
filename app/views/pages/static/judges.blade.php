e25f8864046cb715ad346898a374cab54aa6d11a
lol768 <lol768@lol768.com>
2014-06-01 21:46:41 +0100

    When in doubt add more containers

--- app/views/pages/static/judges.blade.php
+++ app/views/pages/static/judges.blade.php
@@ -1,15 +1,17 @@
 @extends('layouts.master')
 @section('content')
 <div class="content-back">
-    @foreach (Config::get("user-access.present") as $key => $values)
-        <div class="grid-container">
-            <h2>{{{ $key }}}</h2>
-            <?php $i = 0; ?>
-            @foreach ($values as $value)
-                @include("partials.team-entry", array("username" => $value))
-                <?php $i++; ?>
-            @endforeach
-        </div>
-    @endforeach
+    <div class="grid-container"></div>
+        @foreach (Config::get("user-access.present") as $key => $values)
+            <div class="grid-container">
+                <h2>{{{ $key }}}</h2>
+                <?php $i = 0; ?>
+                @foreach ($values as $value)
+                    @include("partials.team-entry", array("username" => $value))
+                    <?php $i++; ?>
+                @endforeach
+            </div>
+        @endforeach
+    </div>
 </div>
 @stop
