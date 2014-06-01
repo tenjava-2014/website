1a8f17ef47b585f193656f79db3cee20f38aa575
lol768 <lol768@lol768.com>
2014-06-01 21:09:54 +0100

    TIL how images worked

--- app/views/partials/team-entry.blade.php
+++ app/views/partials/team-entry.blade.php
@@ -1,7 +1,7 @@
 <div class="grid-20 mobile-grid-50">
     <div class="team-entry text-center">
         <img
-            src="/assets/img/avatars/{{{ strtolower($username) }}}">
+            src="/assets/img/avatars/{{{ strtolower($username) }}}.png">
 
         <h3>{{{ $username }}}</h3>
 
