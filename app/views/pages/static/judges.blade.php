99ca631d1a2c5fa04af130b9da257f329c2dd673
lol768 <lol768@lol768.com>
2014-06-01 21:19:31 +0100

    Deal with having too many damn judges

--- app/views/pages/static/judges.blade.php
+++ app/views/pages/static/judges.blade.php
@@ -4,8 +4,13 @@
     @foreach (Config::get("user-access.present") as $key => $values)
         <div class="grid-container">
             <h2>{{{ $key }}}</h2>
+            <?php $i = 0; ?>
             @foreach ($values as $value)
+                @if ($i % 5 == 0 && $i !== 0)
+                    </div><div class="grid-container top-margin-10">
+                @endif
                 @include("partials.team-entry", array("username" => $value))
+                <?php $i++; ?>
             @endforeach
         </div>
     @endforeach
