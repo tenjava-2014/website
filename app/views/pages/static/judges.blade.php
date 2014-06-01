3fa4a9ad9dafc5db65c836cb864b8ba348d5ddb2
lol768 <lol768@lol768.com>
2014-06-01 20:07:45 +0100

    Continue filling in team profiles

--- app/views/pages/static/judges.blade.php
+++ app/views/pages/static/judges.blade.php
@@ -2,8 +2,8 @@
 @section('content')
 <div class="content-back">
     <div class="grid-container">
+        <!-- ===== ORGANIZERS ===== -->
         <h2>Organizers</h2>
-
         <div class="grid-20">
             <div class="team-entry text-center">
                 <img
@@ -37,6 +37,7 @@
             </div>
         </div>
     </div>
+    <!-- ===== WEB TEAM ===== -->
     <div class="grid-container">
         <h2>Web team</h2>
 
@@ -73,13 +74,25 @@
             </div>
         </div>
     </div>
+    <!-- ===== JUDGES ===== -->
     <div class="grid-container">
         <h2>Judging team</h2>
 
         <div class="grid-20">
             <div class="team-entry text-center">
-                <img 
-                     src="http://www.gravatar.com/avatar/17d0b428a15d8d4f5699dc4d36648f17.jpg?s=200">
+                <img
+                    src="//www.gravatar.com/avatar/efbb451907bcef4423d13af3a8452a83?s=200">
+
+                <h3>jkcclemens</h3>
+
+                <p>bio here</p>
+            </div>
+        </div>
+
+        <div class="grid-20">
+            <div class="team-entry text-center">
+                <img
+                    src="//www.gravatar.com/avatar/17d0b428a15d8d4f5699dc4d36648f17.jpg?s=200">
 
                 <h3>lol768</h3>
 
@@ -89,31 +102,10 @@
 
         <div class="grid-20">
             <div class="team-entry text-center">
-                <img 
-                     src="http://www.gravatar.com/avatar/17d0b428a15d8d4f5699dc4d36648f17.jpg?s=200">
+                <img
+                    src="//www.gravatar.com/avatar/1df81fec8d7c814e69fe2981d4897541?s=200">
 
-                <h3>lol768</h3>
-
-                <p>bio here</p>
-            </div>
-        </div>
-
-        <div class="grid-20">
-            <div class="team-entry text-center">
-                <img 
-                     src="http://www.gravatar.com/avatar/17d0b428a15d8d4f5699dc4d36648f17.jpg?s=200">
-
-                <h3>lol768</h3>
-
-                <p>bio here</p>
-            </div>
-        </div>
-        <div class="grid-20">
-            <div class="team-entry text-center">
-                <img 
-                     src="http://www.gravatar.com/avatar/17d0b428a15d8d4f5699dc4d36648f17.jpg?s=200">
-
-                <h3>lol768</h3>
+                <h3>hawkfalcon</h3>
 
                 <p>bio here</p>
             </div>
@@ -121,10 +113,21 @@
 
         <div class="grid-20">
             <div class="team-entry text-center">
-                <img 
-                     src="http://www.gravatar.com/avatar/17d0b428a15d8d4f5699dc4d36648f17.jpg?s=200">
+                <img
+                    src="//www.gravatar.com/avatar/8af5b07cae6793dc0b2773287a2711f5?s=200">
 
-                <h3>lol768</h3>
+                <h3>lDucks</h3>
+
+                <p>bio here</p>
+            </div>
+        </div>
+
+        <div class="grid-20">
+            <div class="team-entry text-center">
+                <img
+                    src="//www.gravatar.com/avatar/fd0424dd0d490a767761adbaf6f8e036.jpg?s=200">
+
+                <h3>Aero</h3>
 
                 <p>bio here</p>
             </div>
@@ -134,9 +137,9 @@
         <div class="grid-20">
             <div class="team-entry text-center">
                 <img
-                    src="http://www.gravatar.com/avatar/17d0b428a15d8d4f5699dc4d36648f17.jpg?s=200">
+                    src="//i.imgur.com/3vVW3Kf.png?1">
 
-                <h3>lol768</h3>
+                <h3>MasterEjay</h3>
 
                 <p>bio here</p>
             </div>
