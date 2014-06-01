54bb4f02a24d3a2df09c8e19e2ddab891a8cf0f3
lol768 <lol768@lol768.com>
2014-06-01 19:42:07 +0100

    Wrap everything inside a grid-container

--- app/views/pages/static/judges.blade.php
+++ app/views/pages/static/judges.blade.php
@@ -1,46 +1,56 @@
 @extends('layouts.master')
 @section('content')
 <div class="content-back">
+    <div class="grid-container">
+        <div class="grid-20">
+            <div class="info-box about-object text-center">
+                <img style="width: 200px;"
+                     src="http://www.gravatar.com/avatar/17d0b428a15d8d4f5699dc4d36648f17.jpg?s=200">
+                <h4>lol768</h4>
 
-    <div class="grid-20">
-        <div class="info-box about-object text-center">
-            <img style="width: 200px;" src="http://www.gravatar.com/avatar/17d0b428a15d8d4f5699dc4d36648f17.jpg?s=200">
-            <h4>lol768</h4>
-            <p>bio here</p>
+                <p>bio here</p>
+            </div>
+        </div>
+
+        <div class="grid-20">
+            <div class="info-box about-object text-center">
+                <img style="width: 200px;"
+                     src="http://www.gravatar.com/avatar/17d0b428a15d8d4f5699dc4d36648f17.jpg?s=200">
+                <h4>lol768</h4>
+
+                <p>bio here</p>
+            </div>
+        </div>
+
+        <div class="grid-20">
+            <div class="info-box about-object text-center">
+                <img style="width: 200px;"
+                     src="http://www.gravatar.com/avatar/17d0b428a15d8d4f5699dc4d36648f17.jpg?s=200">
+                <h4>lol768</h4>
+
+                <p>bio here</p>
+            </div>
+        </div>
+
+        <div class="grid-20">
+            <div class="info-box about-object text-center">
+                <img style="width: 200px;"
+                     src="http://www.gravatar.com/avatar/17d0b428a15d8d4f5699dc4d36648f17.jpg?s=200">
+                <h4>lol768</h4>
+
+                <p>bio here</p>
+            </div>
+        </div>
+
+        <div class="grid-20">
+            <div class="info-box about-object text-center">
+                <img style="width: 200px;"
+                     src="http://www.gravatar.com/avatar/17d0b428a15d8d4f5699dc4d36648f17.jpg?s=200">
+                <h4>lol768</h4>
+
+                <p>bio here</p>
+            </div>
         </div>
     </div>
-
-    <div class="grid-20">
-        <div class="info-box about-object text-center">
-            <img style="width: 200px;" src="http://www.gravatar.com/avatar/17d0b428a15d8d4f5699dc4d36648f17.jpg?s=200">
-            <h4>lol768</h4>
-            <p>bio here</p>
-        </div>
-    </div>
-
-    <div class="grid-20">
-        <div class="info-box about-object text-center">
-            <img style="width: 200px;" src="http://www.gravatar.com/avatar/17d0b428a15d8d4f5699dc4d36648f17.jpg?s=200">
-            <h4>lol768</h4>
-            <p>bio here</p>
-        </div>
-    </div>
-
-    <div class="grid-20">
-        <div class="info-box about-object text-center">
-            <img style="width: 200px;" src="http://www.gravatar.com/avatar/17d0b428a15d8d4f5699dc4d36648f17.jpg?s=200">
-            <h4>lol768</h4>
-            <p>bio here</p>
-        </div>
-    </div>
-
-    <div class="grid-20">
-        <div class="info-box about-object text-center">
-            <img style="width: 200px;" src="http://www.gravatar.com/avatar/17d0b428a15d8d4f5699dc4d36648f17.jpg?s=200">
-            <h4>lol768</h4>
-            <p>bio here</p>
-        </div>
-    </div>
-
 </div>
 @stop
