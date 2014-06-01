3d3fb52d681cdd2e789b68dcc43dd072f96974eb
lol768 <lol768@lol768.com>
2014-06-01 20:22:15 +0100

    spacing

--- app/lang/en/team-bios.php
+++ app/lang/en/team-bios.php
@@ -1,7 +1,7 @@
 <?php
 return array(
   "lDucks" => "DBO staff, likes web dev and abusing lol768, full-time duck.",
-  "lol768" => "DBO staff, likes web dev and <s>being abused by lDucks</s>writing clean code.",
+  "lol768" => "DBO staff, likes web dev and <s>being abused by lDucks</s> writing clean code.",
   "jkcclemens" => "(Web) Developer, Minecraft & Bukkit enthusiast, and cool guy. Make/maintain plugins in my spare time. Sociable in non-real-life places. The best American organi<strong>z</strong>er.",
   "hawkfalcon" => "DBO staff, bird-ish. Meep. Skwaaawk.",
   "Aero" => "Server owner, web developer",
