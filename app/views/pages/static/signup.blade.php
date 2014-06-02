a13f80272c1727d579f6a452ce1919c9e7a6b8c5
lol768 <lol768@lol768.com>
2014-06-02 17:45:43 +0100

    Work on signup page

--- app/views/pages/static/signup.blade.php
+++ app/views/pages/static/signup.blade.php
@@ -2,9 +2,12 @@
 @section('content')
 <div class="content-back">
     <div class="grid-container">
-        <div class="grid-100">
-            <p>This should essentially be a 'marketing' page on what the contest will involve and aim to encourage people to get involved.</p>
-            <p>The people who click here will be the ones who weren't sure enough to do it directly from the frontpage and want more info.</p>
+        <h2>Why signup?</h2>
+        <div class="grid-70">
+            <h3>Points</h3>
+        </div>
+        <div class="grid-30">
+            <img src="/assets/img/thirdparty/trophy.svg" style="width: 100%">
         </div>
     </div>
 </div>
