97ec225c5d331fe0ca7adab2ece65d6e82bb41ff
lol768 <lol768@lol768.com>
2014-06-02 17:46:17 +0100

    Work on signup page

--- app/views/pages/static/signup.blade.php
+++ app/views/pages/static/signup.blade.php
@@ -2,12 +2,15 @@
 @section('content')
 <div class="content-back">
     <div class="grid-container">
-        <h2>Why signup?</h2>
-        <div class="grid-70">
-            <h3>Points</h3>
-        </div>
-        <div class="grid-30">
-            <img src="/assets/img/thirdparty/trophy.svg" style="width: 100%">
+        <div class="grid-100">
+            <h2>Why signup?</h2>
+
+            <div class="grid-80">
+                <h3>Points</h3>
+            </div>
+            <div class="grid-10">
+                <img src="/assets/img/thirdparty/trophy.svg" style="width: 100%">
+            </div>
         </div>
     </div>
 </div>
