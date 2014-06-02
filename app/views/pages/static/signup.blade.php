14fb30412de103ee7108e3bdf905c45d0f0e2839
lol768 <lol768@lol768.com>
2014-06-02 18:41:46 +0100

    Flesh out signup page a bit more

--- app/views/pages/static/signup.blade.php
+++ app/views/pages/static/signup.blade.php
@@ -11,6 +11,13 @@
             <div class="grid-20">
                 <img src="/assets/img/thirdparty/trophy.svg" style="width: 100%">
             </div>
+
+            <div class="grid-80 push-20">
+                <h3>It's fun!</h3>
+            </div>
+            <div class="grid-20 pull-80">
+                <img src="/assets/img/thirdparty/happy.svg" style="width: 100%">
+            </div>
         </div>
     </div>
 </div>
