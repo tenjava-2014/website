12b7ed19d32d79c3c65422615b7c44248bc5b225
Justin <justin@battleplugins.com>
2014-06-02 21:43:31 -0400

    Alternate font awesome

--- app/views/pages/static/signup.blade.php
+++ app/views/pages/static/signup.blade.php
@@ -4,10 +4,8 @@
     <div class="grid-container">
         <div class="grid-100">
             <h2>Why sign up?</h2>
-
             <div class="grid-80">
                 <h3>Share your creation</h3>
-
                 <p>By taking part in the contest and committing your code into the GitHub repository we create for you,
                     you'll be helping others learn by example. Additionally, we encourage contest winners to consider
                     making their entry into a full project and uploading it to somewhere like <a
@@ -19,21 +17,19 @@
             <div class="grid-20 hide-on-mobile signup-image">
                 <img src="/assets/img/thirdparty/share.svg">
             </div>
+	        <div class="grid-20 hide-on-mobile signup-image">
+		        <img src="/assets/img/thirdparty/trophy.svg">
+	        </div>
             <div class="grid-80">
                 <h3>Points</h3>
-
                 <p>We've already raised {{ number_format($pointsData->points) }} CurseForge points, which is equivalent
                     to ${{ number_format($pointsData->points * 0.05, 2) }}. By participating, you have the chance to win
                     a share of the prize pot. We're splitting the pot in portions of 50%, 30%, and 20% to first, second,
                     and third, respectively. Points can be cashed out via PayPal or redeemed as Amazon gift cards, so
                     it's definitely worth getting involved!</p>
             </div>
-            <div class="grid-20 hide-on-mobile signup-image">
-                <img src="/assets/img/thirdparty/trophy.svg">
-            </div>
             <div class="grid-80">
                 <h3>Learn something new</h3>
-
                 <p>It's our hope that all participants will learn something new in the process of developing their
                     submission. Judges (and hopefully other spectators) will be watching streams and offering
                     advice on code, ideas, and more. We're also trying to ensure all participants get some exposure to
@@ -43,17 +39,15 @@
             <div class="grid-20 hide-on-mobile signup-image">
                 <img src="/assets/img/thirdparty/book.svg">
             </div>
-
+	        <div class="grid-20 hide-on-mobile signup-image">
+		        <img src="/assets/img/thirdparty/happy.svg">
+	        </div>
             <div class="grid-80">
                 <h3>Have fun</h3>
-
                 <p>Above all, we're hoping ten.java will be a fun experience for everyone involved. We enjoyed judging
                     the entries last year and seeing everything that the participants came up with, and we are looking
                     forward to doing the same on a larger scale this year. For participants, ten.java should be a chance
                     to experiment and hopefully learn something in the process.</p>
-            </div>
-            <div class="grid-20 hide-on-mobile signup-image">
-                <img src="/assets/img/thirdparty/happy.svg">
             </div>
         </div>
     </div>
