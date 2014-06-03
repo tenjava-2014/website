08658cc37cf5615907657f0fe79103b11866e11b
lol768 <lol768@lol768.com>
2014-06-03 23:34:47 +0100

    Add error handle

--- app/controllers/TimeController.php
+++ app/controllers/TimeController.php
@@ -3,9 +3,20 @@
 class TimeController extends BaseController {
 
     public function showUserTimes() {
-        $appData = Session::get("application_data");
         $githubUsername = $this->auth->getUsername();
+        $ghId = $this->auth->getUserId();
+        $appCount = Application::where("gh_id", $ghId)->where("judge", false)->count();
+        if ($appCount == 0) {
+            $errorController = App::make("ErrorController");
+            return $errorController->priorapp();
+        }
         return View::make("pages.forms.time-selection")->with(array("username" => $githubUsername));
+    }
+
+    public function confirmUserTimes() {
+        $ghId = $this->auth->getUserId();
+        $app = Application::where("gh_id", $ghId)->first();
+
     }
 
 } 
---
+++ app/views/errors/priorapp.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+@section('content')
+<div class="content-back">
+    <div class="grid-container">
+        <div class="grid-100">
+            <h2>Registration required</h2>
+            <p>You must signup as a participant first.</p>
+        </div>
+    </div>
+</div>
+@stop
