6eb287afe628138ba9e9d082a191284cfbd7b1d8
lol768 <lol768@lol768.com>
2014-06-03 21:12:44 +0100

    Use correct view

--- app/controllers/TimeController.php
+++ app/controllers/TimeController.php
@@ -5,7 +5,7 @@ class TimeController extends BaseController {
     public function showUserTimes() {
         $appData = Session::get("application_data");
         $githubUsername = $this->auth->getUsername();
-        return View::make("time-selection")->with(array("username" => $githubUsername));
+        return View::make("pages.forms.time-selection")->with(array("username" => $githubUsername));
     }
 
 } 
