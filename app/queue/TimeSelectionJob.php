90358c23635a6d42125664870da7b324f00b97f6
lol768 <lol768@infinitenight.net>
2014-06-04 00:53:40 +0100

    Delete the job once it's done, prevent repeated execution

--- app/queue/TimeSelectionJob.php
+++ app/queue/TimeSelectionJob.php
@@ -14,6 +14,7 @@ class TimeSelectionJob {
         if ($data['t3']) {
             $this->addUserRepo($data['username'] . "-t3", $client);
         }
+        $job->delete();
     }
 
     public function addUserRepo($username, $client) {
