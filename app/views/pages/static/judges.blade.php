facf2a024df549a8abdb00c67ed47766f6bc5a72
Justin <justin@battleplugins.com>
2014-06-02 21:38:16 -0400

    Attempt to fix judge page

--- app/views/pages/static/judges.blade.php
+++ app/views/pages/static/judges.blade.php
@@ -1,17 +1,20 @@
 @extends('layouts.master')
 @section('content')
+@foreach (Config::get("user-access.present") as $key => $values)
+
+<h2>{{{ $key }}}</h2>
+<?php $i = 0; ?>
+@foreach ($values as $value)
+@if($i % 2 > 0)
 <div class="content-back">
-    <div class="grid-container"></div>
-        @foreach (Config::get("user-access.present") as $key => $values)
-            <div class="grid-container">
-                <h2>{{{ $key }}}</h2>
-                <?php $i = 0; ?>
-                @foreach ($values as $value)
-                    @include("partials.team-entry", array("username" => $value))
-                    <?php $i++; ?>
-                @endforeach
-            </div>
-        @endforeach
-    </div>
+	@endif
+	<div class="grid-container">
+		@include("partials.team-entry", array("username" => $value))
+	</div>
+	@if($i % 2 > 0)
 </div>
+@endif
+<?php $i++; ?>
+@endforeach
+@endforeach
 @stop
