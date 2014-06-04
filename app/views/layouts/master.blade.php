54e38ad72cd588218fa4785d08f2023bbc3a0e51
lol768 <lol768@infinitenight.net>
2014-06-04 01:07:31 +0100

    Convert to ms

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -34,7 +34,7 @@ $( document ).ready(function() {
         $("#nav-container").toggleClass("hide-on-mobile");
     });
     $(".date-replacer").each(function() {
-        var date = new Date($(this).data("time"));
+        var date = new Date($(this).data("time") * 1000);
         $(this).text(date.toString());
     });
 });
