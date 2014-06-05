daefbbb71b5b0be51dce57ec4aef308551f386db
lol768 <lol768@lol768.com>
2014-06-05 22:37:24 +0100

    Add email chooser

--- app/commands/MailReminderCommand.php
+++ app/commands/MailReminderCommand.php
@@ -40,7 +40,27 @@ class MailReminderCommand extends Command {
             $this->info("Checking " . $app->gh_username);
             $timeEntry = $app->timeEntry()->first();
             if ($timeEntry === null) {
-                $this->info("No time entry!");
+                $this->comment("No time entry!");
+                $emails = json_decode($app->emails, true);
+                if (array_key_exists("fail", $emails)) {
+                    $this->error($app->gh_username . " has no email entry (user declined). Contact DBO: " . $app->dbo_username);
+                } else if (array_key_exists("others", $emails)) {
+                    $emails = $emails['others'];
+                }
+                $chosenEmail = null;
+                foreach ($emails as $email) {
+                    if (str_contains($email, "users.noreply")) {
+                        continue;
+                    } else {
+                        $chosenEmail = $email;
+                        break;
+                    }
+                }
+                if ($chosenEmail === null) {
+                    $this->error($app->gh_username . " has no email entry (none available). Contact DBO: " . $app->dbo_username);
+                } else {
+                    $this->comment("Chosen email is " . $chosenEmail);
+                }
             } else {
                 $this->info("Valid time entry.");
             }
