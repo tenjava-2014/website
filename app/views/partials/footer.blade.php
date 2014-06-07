8150db70318269b4de20ce6d9073c1c30b130148
lol768 <lol768@lol768.com>
2014-06-07 19:36:37 +0100

    Use right email address, prevent bots scraping

--- app/views/partials/footer.blade.php
+++ app/views/partials/footer.blade.php
@@ -8,7 +8,7 @@
 		</div>
 		<div class="grid-20 tablet-grid-50 mobile-grid-100">
 			<ul class="social-media">
-				<li><a href="mailto:contact@tenjava.com"><i class="fa fa-2x fa-envelope-square"></i></a></li>
+				<li><a href="{{{ HTML::obfuscate('tenjava@' . 'ten' . 'java.com') }}}"><i class="fa fa-2x fa-envelope-square"></i></a></li>
 				<li><a title="" target="_blank" href="https://github.com/tenjava"><i class="fa fa-2x fa-github-square"></i></a></li>
 				<li><a target="_blank" href="https://twitter.com/tenjava"><i class="fa fa-2x fa-twitter-square"></i></a></li>
 				<li><a target="_blank" href="http://forums.bukkit.org/threads/269253/"><span class="fa-stack bottom"><i class="fa fa-square fa-stack-2x"></i><i class="fa fa-comment fa-stack-1x black"></i></span></a></li>
