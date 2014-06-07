71841823feca72e64fc94c1154e0bd7d70f5c8e4
lol768 <lol768@lol768.com>
2014-06-07 22:56:00 +0100

    Better env detection

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -24,12 +24,15 @@ $app = new Illuminate\Foundation\Application;
 |
 */
 
-$env = $app->detectEnvironment(array(
-
-	'local' => array('windows8'),
-    'prod' => array('')
-
-));
+$env = $app->detectEnvironment(function () {
+    if (str_contains($_SERVER['HTTP_HOST'], "beta")) {
+        return "beta";
+    } else if (str_contains($_SERVER['HTTP_HOST'], "dev")) {
+        return "dev";
+    } else {
+        return "prod";
+    }
+});
 
 /*
 |--------------------------------------------------------------------------
@@ -42,7 +45,7 @@ $env = $app->detectEnvironment(array(
 |
 */
 
-$app->bindInstallPaths(require __DIR__.'/paths.php');
+$app->bindInstallPaths(require __DIR__ . '/paths.php');
 
 /*
 |--------------------------------------------------------------------------
@@ -55,10 +58,10 @@ $app->bindInstallPaths(require __DIR__.'/paths.php');
 |
 */
 
-$framework = $app['path.base'].
-                 '/vendor/laravel/framework/src';
+$framework = $app['path.base'] .
+    '/vendor/laravel/framework/src';
 
-require $framework.'/Illuminate/Foundation/start.php';
+require $framework . '/Illuminate/Foundation/start.php';
 
 /*
 |--------------------------------------------------------------------------
