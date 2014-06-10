8c626cbf2437eae3be2a6588a55ac65311d4a298
lol768 <lol768@lol768.com>
2014-06-10 21:23:34 +0100

    Switch arg order

--- app/models/Application.php
+++ app/models/Application.php
@@ -9,7 +9,7 @@ class Application extends \Illuminate\Database\Eloquent\Model {
 
     public function formatEmails() {
         $emails = $this->github_email;
-        if (array_key_exists("public", $emails)) {
+        if (array_key_exists($emails, "public")) {
             return $emails;
         } else {
             return implode(",", $emails);
