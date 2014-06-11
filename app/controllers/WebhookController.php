17c9af3ca72bc76db4f50be9fe47c197c0994526
lol768 <lol768@lol768.com>
2014-06-11 18:34:58 +0100

    Handle GitHub's header format

--- app/controllers/WebhookController.php
+++ app/controllers/WebhookController.php
@@ -10,7 +10,11 @@ class WebhookController extends BaseController {
     }
 
     public function processGitHubWebhook() {
-        if (!$this->hmac->verifySignature(Input::instance()->getContent(), Request::header("X-Hub-Signature"), Config::get("webhooks.secret"))) {
+        $header = Request::header("X-Hub-Signature");
+        parse_str($header, $output);
+        $header = $output['sha1'];
+
+        if (!$this->hmac->verifySignature(Input::instance()->getContent(), $header, Config::get("webhooks.secret"))) {
             return Response::json("Invalid HMAC signature.");
         } else {
             return Response::json("OK");
