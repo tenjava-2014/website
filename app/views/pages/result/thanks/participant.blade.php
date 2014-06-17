ed4ce62e11a9e0e657540d938ec6c809193bfc12
lol768 <lol768@lol768.com>
2014-06-17 18:52:23 +0100

    Add in additional information during sign-up process

--- app/views/pages/result/thanks/participant.blade.php
+++ app/views/pages/result/thanks/participant.blade.php
@@ -3,14 +3,29 @@
 <div class="content-back">
     <div class="grid-container">
         <div class="grid-100">
-            <h2>You're all set!</h2>
+            <h2>You're almost done!</h2>
 
-            <p>Thanks for applying for ten.java 2014. You've now completed your registration. You may now <a href="/times/select">choose</a> your contest time.</p>
+            <div class="alert basic error"><p><strong>Important!</strong> You will not be able to participate unless you
+                    select a time. You should do this as soon as possible.</p>
+            </div>
+            <p>Thanks for singing up as a participant. You may now <a
+                    href="/times/select">choose</a> your contest timeslot.</p>
 
             <h3>Share to twitter</h3>
-            <p>If you'd like to get more people involved you can share your sign-up to twitter using the button below:</p>
-            <a href="https://twitter.com/intent/tweet?screen_name=tenjava&text=I%20just%20signed%20up%20for%20ten.java%202014!" class="twitter-mention-button" data-related="tenjava" data-dnt="true">Share my registration</a>
-            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+
+            <p>If you'd like to get more people involved you can share your sign-up to twitter using the button
+                below:</p>
+            <a href="https://twitter.com/intent/tweet?screen_name=tenjava&text=I%20just%20signed%20up%20for%20ten.java%202014!"
+               class="twitter-mention-button" data-related="tenjava" data-dnt="true">Share my registration</a>
+            <script>!function (d, s, id) {
+                    var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https';
+                    if (!d.getElementById(id)) {
+                        js = d.createElement(s);
+                        js.id = id;
+                        js.src = p + '://platform.twitter.com/widgets.js';
+                        fjs.parentNode.insertBefore(js, fjs);
+                    }
+                }(document, 'script', 'twitter-wjs');</script>
         </div>
     </div>
 </div>
--- app/views/pages/result/thanks/times.blade.php
+++ app/views/pages/result/thanks/times.blade.php
@@ -4,7 +4,7 @@
     <div class="grid-container">
         <div class="grid-100">
             <h2>Thanks!</h2>
-            <p>You've confirmed your time selection.</p>
+            <p>You've confirmed your time selection. We've made you a GitHub repository and you're now ready.You might want to read our <a href="https://tenjava.com/wiki">wiki</a> so you know you're ready for the contest.</p>
             <!-- {{{ $entry->t1 }}} {{{ $entry->t2 }}} {{{ $entry->t3 }}} -->
         </div>
     </div>
