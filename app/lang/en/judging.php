4264e4bba22a66ff9e563cd613fa14c5373799b0
lol768 <lol768@lol768.com>
2014-06-29 17:11:16 +0100

    Message consistency

--- app/lang/en/judging.php
+++ app/lang/en/judging.php
@@ -1,5 +1,5 @@
 <?php
 return array(
-    "pr-warning" => ":warning: **WARNING**\n\nPer the [official rules](https://github.com/tenjava/official-rules/blob/master/README.md#during-the-contest), accepting this or any other pull request will result in immediate disqualification. The creation of this PR has been reported to the judging team for review. Furthermore, this issue has automatically been closed.",
+    "pr-warning" => ":warning: **WARNING**\n\nPer the [official rules](https://github.com/tenjava/official-rules/blob/master/README.md#during-the-contest), accepting this or any other pull request will result in immediate disqualification. The creation of this PR has been reported to the judging team for review. Furthermore, this PR will now be automatically closed.",
     "pr-warning-2" => ":exclamation: **Please stop disregarding these automated messages**\n\nAs we've said, merging a PR is **against the [official rules](https://github.com/tenjava/official-rules/blob/master/README.md#during-the-contest)** and **will result in disqualification**. You've been warned multiple times. This PR will now be automatically closed."
 );
