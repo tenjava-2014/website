03d2ac57af85b94b0ccf350f655a569ae07b9eee
lol768 <lol768@lol768.com>
2014-06-29 20:48:41 +0100

    Migrate team page to database

--- app/TenJava/Controllers/Pages/TeamController.php
+++ app/TenJava/Controllers/Pages/TeamController.php
@@ -1,14 +1,21 @@
 <?php
 namespace TenJava\Controllers\Pages;
 
+use Config;
 use Response;
 use TenJava\Controllers\Abstracts\BaseController;
+use TenJava\Models\Judge;
 
 class TeamController extends BaseController {
 
     public function showTeam() {
         $this->setPageTitle("Meet the team");
         $this->setActive("team");
+        $teamMembers = array();
+        $teamMembers['Organizers'] = Judge::where("admin", true)->lists("github_name");
+        $teamMembers['Web Team'] = Judge::where("web_team", true)->lists("github_name");
+        $teamMembers['Judges'] = Judge::lists("github_name");
+        $teamMembers['Sponsors'] = Config::get("user-access.present.Sponsors");
         return Response::view('pages.static.judges', array());
     }
 
