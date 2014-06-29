6d3325d122712bfa960898d0087060b1a539b59e
lol768 <lol768@lol768.com>
2014-06-29 20:13:35 +0100

    Add required service provider

--- app/config/packages/frozennode/administrator/administrator.php
+++ app/config/packages/frozennode/administrator/administrator.php
@@ -102,14 +102,14 @@ return array(
 	 *
 	 * @type string
 	 */
-	'login_path' => 'user/login',
+	'login_path' => '/login',
 
 	/**
 	 * The logout path is the path where Administrator will send the user when they click the logout link
 	 *
 	 * @type string
 	 */
-	'logout_path' => false,
+	'logout_path' => '/logout',
 
 	/**
 	 * This is the key of the return path that is sent with the redirection to your login_action. Session::get('redirect') will hold the return URL.
