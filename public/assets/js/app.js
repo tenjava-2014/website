57bacfd15d70b7244f7f6b4bb028386fbc06d1ea
lol768 <lol768@lol768.com>
2014-07-04 21:55:18 +0100

    This makes so much sense, I love JavaScript

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,5 +1,5 @@
 function formatDate(date) {
-    return date.getFullYear() + "-" + ('0' + (date.getMonth() + 1)).slice(-2) + "-" + ('0' + date.getDay()).slice(-2) + " " + ('0' + date.getHours()).slice(-2) + ":" + ('0' + date.getMinutes()).slice(-2) + ":" + ('0' + date.getSeconds()).slice(-2);
+    return date.getFullYear() + "-" + ('0' + (date.getMonth() + 1)).slice(-2) + "-" + ('0' + date.getDate()).slice(-2) + " " + ('0' + date.getHours()).slice(-2) + ":" + ('0' + date.getMinutes()).slice(-2) + ":" + ('0' + date.getSeconds()).slice(-2);
 }
 
 
