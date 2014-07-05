f8567247cd12a3b1f71826850eb42e191de399cc
lol768 <lol768@lol768.com>
2014-07-05 23:23:27 +0100

    More lenient CSP

--- app/TenJava/ServiceProvider/TenJava.php
+++ app/TenJava/ServiceProvider/TenJava.php
@@ -120,7 +120,7 @@ class TenJava extends ServiceProvider {
                     "default-src 'self'; " .
                     "style-src 'self' https://cdnjs.cloudflare.com https://fonts.googleapis.com 'unsafe-inline'; " .
                     "font-src 'self' https://cdnjs.cloudflare.com themes.googleusercontent.com; " .
-                    "img-src 'self'; " . // this will likely need changing for twitch
+                    "img-src 'self' githubusercontent.com static-cdn.jtvnw.net; " . // this will likely need changing for twitch
                     "media-src 'self'; " . // this will likely need changing for twitch
                     "object-src 'self'; " . // this will likely need changing for twitch
                     "script-src 'self' https://cdnjs.cloudflare.com https://platform.twitter.com" . $unsafes
@@ -132,7 +132,7 @@ class TenJava extends ServiceProvider {
                     "default-src 'self'; " .
                     "style-src 'self' cdnjs.cloudflare.com fonts.googleapis.com 'unsafe-inline'; " .
                     "font-src 'self' cdnjs.cloudflare.com themes.googleusercontent.com; " .
-                    "img-src 'self'; " . // this will likely need changing for twitch
+                    "img-src 'self' githubusercontent.com static-cdn.jtvnw.net; " . // this will likely need changing for twitch
                     "media-src 'self'; " . // this will likely need changing for twitch
                     "object-src 'self'; " . // this will likely need changing for twitch
                     "script-src 'self' cdnjs.cloudflare.com platform.twitter.com" . $unsafes
