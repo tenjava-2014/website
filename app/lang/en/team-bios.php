e0c998bd7fe2df35fcfdbf9f6bc3ce27d2d19c71
lol768 <lol768@lol768.com>
2014-07-07 18:31:45 +0100

    Manually merge dpasi314

--- app/lang/en/team-bios.php
+++ app/lang/en/team-bios.php
@@ -20,5 +20,6 @@ return array(
   "Twister915" => "Experienced Java developer, and owner of TBNR.net and Bashur's World.com, Twister915 AKA Joey S is both a Minecraft and Bukkit enthusiast.",
   "rigor789" => "Java/Web developer, graphics/UX designer, and an experienced system operator. Developer at TBNR.net! I love clean code, and I hate writing tests.",
   "Bionicrm" => "A Java/web developer, DBO staff member, computer enthusiast, and lol768 fan club member.",
-  "Intreppid" => "Intreppid has kindly provided us with a dedicated server that we'll be using to build and test all submissions. See the About page for more."
+  "Intreppid" => "Intreppid has kindly provided us with a dedicated server that we'll be using to build and test all submissions. See the About page for more.",
+  "Dpasi314" => "Java / Bukkit / ShockNetwork Developer. I like cats, programming, Skyrim, Lindsey Stirling and lol768!"
 );
