8c3068a3a9cb448e7d38a38973de6254e4744fb5
lol768 <lol768@lol768.com>
2014-07-09 22:45:59 +0100

    Does this give me a legend?

--- public/assets/js/charts.js
+++ public/assets/js/charts.js
@@ -24,6 +24,8 @@ $(function() {
 
     ];
 
-    new Chart(ctx).PolarArea(data);
+    new Chart(ctx).PolarArea(data, {
+        legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>"
+    });
 
 });
