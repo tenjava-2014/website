8a2fed03ea8f310c77f5e3cc6e53dcdf1b659543
lol768 <lol768@lol768.com>
2014-07-09 23:28:05 +0100

    Use actual dates

--- public/assets/js/charts.js
+++ public/assets/js/charts.js
@@ -42,7 +42,7 @@ $(function() {
             if ($.inArray(curseTimestamp, timestamps) == -1) {
                 console.log("Dealing with " + item['username'] + " with a CT of " + curseTimestamp + " and amount of " + item.amount);
                 var dateLbl = getDateLabel(new Date(curseTimestamp * 1000));
-                dateLbl = index;
+                //dateLbl = index;
                 timestampLabels.push(dateLbl);
                 timestamps.push(curseTimestamp);
                 var curVal = values[curseTimestamp];
