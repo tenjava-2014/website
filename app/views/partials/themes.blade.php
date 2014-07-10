104b0d33959724f2bdf5a27dbf8705349e8f735e
lol768 <lol768@lol768.com>
2014-07-10 22:11:54 +0100

    Fix time checks

--- app/views/partials/themes.blade.php
+++ app/views/partials/themes.blade.php
@@ -3,19 +3,19 @@
     need to choose one of the themes from your timeslot and note it in the README file. This should be the first
     thing you do at the start of your timeslot. Unsure which timeslot you selected? See <a href="/times/select">this page</a>.</p>
 <h2>Timeslot 1 ({{ $t1 }})</h2>
-@if ($times->isT1Active())
+@if ($times->isT1Active() || $times->isT1Finished())
     <p>Yay a theme would go here</p>
 @else
     <p>The themes for this timeslot are not yet available as it has not yet started.</p>
 @endif
 <h2>Timeslot 2 ({{ $t2 }})</h2>
-@if ($times->isT1Active())
+@if ($times->isT2Active() || $times->isT2Finished())
     <p>Yay a theme would go here</p>
 @else
     <p>The themes for this timeslot are not yet available as it has not yet started.</p>
 @endif
 <h2>Timeslot 3 ({{ $t3 }})</h2>
-@if ($times->isT1Active())
+@if ($times->isT3Active() || $times->isT3Finished())
     <p>Yay a theme would go here</p>
 @else
     <p>The themes for this timeslot are not yet available as it has not yet started.</p>
