18567d26dc72bc08ec80455fc6643bba4b5fb260
lol768 <lol768@lol768.com>
2014-07-10 17:03:17 +0100

    Add link to twitch page

--- app/views/pages/dynamic/twitch.blade.php
+++ app/views/pages/dynamic/twitch.blade.php
@@ -5,7 +5,7 @@
     @foreach ($twitch as $entry)
         <div class="grid-20 mobile-grid-20 tablet-grid-20 twitch">
             <div class="twitch-snapshot">
-                <img src="{{{ str_replace(['{WIDTH}', '{HEIGHT}'], ['600', '400'], $entry->onlineStream->preview_template) }}}">
+                <a href="https://twitch.tv/{{{ $entry->twitch_username }}}"><img src="{{{ str_replace(['{WIDTH}', '{HEIGHT}'], ['600', '400'], $entry->onlineStream->preview_template) }}}"></a>
             </div>
             <div class="twitch-footer">
                 {{{ $entry->gh_username }}}'s stream
