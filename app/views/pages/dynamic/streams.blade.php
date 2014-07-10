f03dd572efdd017ba0dc8bf94dae256a432151fc
lol768 <lol768@lol768.com>
2014-07-10 18:15:00 +0100

    Syntax error fix

--- app/views/pages/dynamic/streams.blade.php
+++ app/views/pages/dynamic/streams.blade.php
@@ -10,7 +10,7 @@
                 <p>This page lists all online streams. This data was last updated {{{ $twitch[0]->created_at->diffForHumans() }}}.</p>
             @endif
         </div>
-        @if (count($twitch != 0)
+        @if (count($twitch) != 0)
             @include("partials.twitch", array("full" => true))
         @endif
     </div>
