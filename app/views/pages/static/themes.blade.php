77f44b6938c9b09f67f5011761cb35172af1f16a
lol768 <lol768@lol768.com>
2014-07-10 20:55:15 +0100

    Add final info email

--- app/views/pages/static/themes.blade.php
+++ app/views/pages/static/themes.blade.php
@@ -2,7 +2,11 @@
 @section('content')
 <div class="content-back">
     <div class="grid-container">
-        Themes will be announced when the contest starts via Twitter and IRC
+        <h2>Themes</h2>
+
+        <p>As each timeslot starts, this page will list the available themes for each one. If you're participating, you
+            need to choose one of the themes from your timeslot and note it in the README file. This should be the first
+            thing you do.</p>
     </div>
 </div>
 @stop
