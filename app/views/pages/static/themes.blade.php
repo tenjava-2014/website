e47ba921a7a924c210e790343353af0c53968dec
lol768 <lol768@lol768.com>
2014-07-10 20:57:13 +0100

    Add some more info to the themes page

--- app/views/pages/static/themes.blade.php
+++ app/views/pages/static/themes.blade.php
@@ -6,7 +6,7 @@
 
         <p>As each timeslot starts, this page will list the available themes for each one. If you're participating, you
             need to choose one of the themes from your timeslot and note it in the README file. This should be the first
-            thing you do.</p>
+            thing you do at the start of your timeslot. Unsure which timeslot you selected? See <a href="/times/select">this page</a>.</p>
     </div>
 </div>
 @stop
