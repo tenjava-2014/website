238c80abe52e91e4b63068aadc7d7c48490f055b
lol768 <lol768@lol768.com>
2014-07-10 23:33:18 +0100

    Add more time info

--- app/views/emails/final-info.blade.php
+++ app/views/emails/final-info.blade.php
@@ -14,7 +14,7 @@ You’ve signed up for time 2. Time 2 runs from 07/12/2014 09:00:00 to 07/12/201
 You’ve signed up for time 3. Time 3 runs from 07/12/2014 14:00:00 to 07/13/2014 00:00:00 (UTC). All dates are in mm/dd/yyyy form. If you’d like to see your selected time in your local timezone, visit https://tenjava.com/times/select. We also have a countdown on https://tenjava.com.
 @endif
 @if (!$t1 && !$t2 && !$t3)
-    You haven’t yet chosen a time. If you don’t choose by Friday July 11th 22:00, you’ll be unable to participate in the ten.java contest and your entry will be discarded. To choose a time, visit https://tenjava.com/times/select.
+    You haven’t yet chosen a time. If you don’t choose by Friday July 11th at 22:00 (UTC), you’ll be unable to participate in the ten.java contest and your entry will be discarded. To choose a time, visit https://tenjava.com/times/select.
 @endif
 
 When the contest starts, themes will be available to view at https://tenjava.com/themes and will be revealed at the start of each timeslot. Push access will be granted shortly before the start of each timeslot.
