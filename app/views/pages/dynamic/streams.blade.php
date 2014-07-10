634ed74105e08a0fa5759ef5e3c7cfb27142d824
lol768 <lol768@lol768.com>
2014-07-10 18:11:50 +0100

    Add last updated info

--- app/views/pages/dynamic/streams.blade.php
+++ app/views/pages/dynamic/streams.blade.php
@@ -7,6 +7,7 @@
             @if (count($twitch) == 0)
                 <p>There are no online streams.</p>
             @else
+                <p>This page lists all online streams. This data was last updated {{{ $twitch[0]->created_at->diffForHumans() }}}.</p>
                 @include("partials.twitch", array("full" => true))
             @endif
         </div>
