b603ef88171286f66cbf7122b29c03f6441819c6
lol768 <lol768@lol768.com>
2014-07-11 19:12:19 +0100

    Put it in an array

--- app/views/pages/dynamic/streams.blade.php
+++ app/views/pages/dynamic/streams.blade.php
@@ -16,7 +16,7 @@
         <div class="grid-100">
             <h2>Other streaming platforms</h2>
             <p>Some participants have elected to use an alternative streaming platform in place of twitch.tv. We don't check the status of these streams, but provide a list below for convenience:</p>
-            @include("partials.generic-stream", array("streams" => ["url" => "http://www.hitbox.tv/thekomputerking", "preview" => "http://edge.sf.hitbox.tv/static/img/live/thekomputerking_large_000.jpg", "name" => "TheKomputerKing"]))
+            @include("partials.generic-stream", array("streams" => [["url" => "http://www.hitbox.tv/thekomputerking", "preview" => "http://edge.sf.hitbox.tv/static/img/live/thekomputerking_large_000.jpg", "name" => "TheKomputerKing"]]))
         </div>
     </div>
 </div>
