0ef7ae77da328b5b3988077e9d0582ab0543177a
lol768 <lol768@lol768.com>
2014-07-11 19:11:34 +0100

    Add generic streams

---
+++ app/views/partials/generic-stream.blade.php
@@ -0,0 +1,12 @@
+<div>
+    @foreach ($streams as $entry)
+        <div class="grid-20 mobile-grid-20 tablet-grid-20 twitch">
+            <div class="twitch-snapshot">
+                <a href="{{ $entry['url'] }}"><img src="{{ $entry['preview'] }}"></a>
+            </div>
+            <div class="twitch-footer">
+                {{ $entry['username'] }}'s stream
+            </div>
+        </div>
+    @endforeach
+</div>
