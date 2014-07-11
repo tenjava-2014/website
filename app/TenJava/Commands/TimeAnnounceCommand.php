a5bbe18435d0d34b91acf703c12945dd5a7a2679
lol768 <lol768@lol768.com>
2014-07-11 21:44:00 +0100

    Add participant reg notification

--- app/TenJava/Commands/TimeAnnounceCommand.php
+++ app/TenJava/Commands/TimeAnnounceCommand.php
@@ -84,6 +84,7 @@ class TimeAnnounceCommand extends Command {
             "the end of timeslot 1" => $this->times->getT1EndTime(),
             "the end of timeslot 2" => $this->times->getT2EndTime(),
             "the end of timeslot 3" => $this->times->getT3EndTime(),
+            "participant registrations/time choices close" => 1405116000
         );
         while (time() < $this->times->getT3EndTime()) {
             foreach ($times as $name => $ts) {
