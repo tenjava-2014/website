6834cafb988b7ce6d67e9157759c762341fe895e
lol768 <lol768@lol768.com>
2014-07-15 17:41:00 +0100

    Add deadline info

--- app/views/judging/pages/dashboard.blade.php
+++ app/views/judging/pages/dashboard.blade.php
@@ -9,6 +9,7 @@
                 a judging team to decide who wins. We've built this judging interface to try and make judging as
                 painless
                 as possible. If you have any suggestions or questions, please direct them to lol768.</p>
+
             <h2>Plugin assignment overview</h2>
 
             <p>We've automatically assigned all judges with a set of plugins such that each entry is judged exactly
@@ -28,12 +29,34 @@
                 information on the CraftBukkit/Java versions you should be using so that we can try to ensure test
                 environments are consistent.</p>
 
-
-
             <div class="server-details"><strong>Server IP:</strong> thor.tenjava.com:{{{ $judgePort }}}</div>
 
             <p>For information on how to use the tools available to you on the judging servers, see the <a
                     href="/judging/help">help</a> page.</p>
+
+            <h2>Maintaining a presence on IRC</h2>
+
+            <p>As IRC is our primary communications medium, all judges should be making an effort to be available via
+                IRC whenever possible during the judging period. This hasn't yet been a significant issue with most
+                judges. If you aren't already using an IRC bouncer, you should look into obtaining one.</p>
+        </div>
+    </div>
+</div>
+<div class="grid-container">
+    <div class="grid-100 grid-parent">
+        <div class="grid-20 hide-on-mobile signup-image">
+            <img src="/assets/img/thirdparty/clock.svg" alt="Clock icon">
+        </div>
+        <div class="grid-80">
+            <h3>Judging deadline</h3>
+
+            <p>We'd like all judges to try and ensure they have judged all of their assigned plugins by July 31st. We
+                cannot guarantee that we will be able to provide testing/build servers beyond this date.</p>
+
+            <!-- This is NOT necessarily when results will be announced for those looking into the code behind our judging system -->
+
+            <p>If at any point you are unsure you will be able to complete all of your assigned entries, please contact
+                an organizer <strong>as soon as possible</strong>.</p>
         </div>
     </div>
 </div>
