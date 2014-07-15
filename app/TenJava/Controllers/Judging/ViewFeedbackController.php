4cfe86ab6ba6e8e8356768351cd1c37eb437ba65
lol768 <lol768@lol768.com>
2014-07-15 20:48:54 +0100

    Use an associative array for view data

--- app/TenJava/Controllers/Judging/ViewFeedbackController.php
+++ app/TenJava/Controllers/Judging/ViewFeedbackController.php
@@ -13,7 +13,7 @@ class ViewFeedbackController extends BaseJudgingController {
     public function showFeedback() {
         $feedbacks = ParticipantFeedback::with("participant")->get();
         $this->setPageTitle("Feedback viewer");
-        return View::make("judging.pages.feedback", ["feedbacks", $feedbacks]);
+        return View::make("judging.pages.feedback", ["feedbacks" => $feedbacks]);
     }
 
 } 
