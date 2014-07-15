42924141f3f42e86c3b8699978fc2426dcc324fe
lol768 <lol768@lol768.com>
2014-07-15 20:58:33 +0100

    Add link to whoever left the feedback

--- app/views/judging/pages/feedback.blade.php
+++ app/views/judging/pages/feedback.blade.php
@@ -6,7 +6,10 @@
             <h1>Feedback</h1>
             <p>This page shows all feedback submitted by participants.</p>
             @foreach ($feedbacks as $item)
-                <blockquote class="feedback"><p>{{{ $item->comment }}}</p></blockquote>
+                <blockquote class="feedback">
+                    <p>{{{ $item->comment }}}</p>
+                    <span class="username"><a href="/list/search?search={{{ $item->participant->gh_username }}}">{{{ $item->participant->gh_username }}}</a></span>
+                </blockquote>
             @endforeach
         </div>
     </div>
