a110294b54cda6e720b48ad6b62bcbfb9082b209
lol768 <lol768@lol768.com>
2014-07-15 22:53:43 +0100

    Add case

--- app/views/judging/pages/judge.blade.php
+++ app/views/judging/pages/judge.blade.php
@@ -4,12 +4,7 @@
     <div class="grid-container">
         <div class="grid-100">
             <h1>Judging {{{ $claim->repo_name }}}</h1>
-            <p>Girl of a boldly honor, accelerate the alarm! The nanomachine is more vogon now than transporter. galactic and cunningly interstellar.
-                red alert. make it so. tragedy at the holodeck was the love of galaxy, converted to a colorful proton.
-                vision at the moon that is when vital stars meet. this coordinates has only been placed by a colorful queen.
-                star of a greatly exaggerated alarm, manifest the hypnosis! dozens of assimilations will be lost in stigmas like loves in minerals
-                when the moon harvests for astral city, all planets invade apocalyptic, proud particles. the cloudy kahless bravely opens the space.
-                remarkable, neutral astronauts rudely eat a reliable, unrelated spacecraft. all those attitudes will be lost in assimilations like mysteries in deaths</p>
+            <p>Girl of a boldly honor, accelerate the alarm! The nanomachine is more vogon now than transporter. Galactic and cunningly interstellar. Red alert. Make it so. Tragedy at the holodeck was the love of galaxy, converted to a colorful proton. Vision at the moon that is when vital stars meet. This coordinates has only been placed by a colorful queen. Star of a greatly exaggerated alarm, manifest the hypnosis! Dozens of assimilations will be lost in stigmas like loves in minerals when the moon harvests for astral city, all planets invade apocalyptic, proud particles. The cloudy kahless bravely opens the space. Remarkable, neutral astronauts rudely eat a reliable, unrelated spacecraft. All those attitudes will be lost in assimilations like mysteries in deaths.</p>
         </div>
     </div>
 </div>
