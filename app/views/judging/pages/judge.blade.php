cf79d58a09fc13282dc3dfffd1c8a1312549b5e2
lol768 <lol768@lol768.com>
2014-07-15 22:57:09 +0100

    Add cats

--- app/views/judging/pages/judge.blade.php
+++ app/views/judging/pages/judge.blade.php
@@ -2,11 +2,11 @@
 @section('content')
 <div class="content-back">
     <div class="grid-container">
-        <div class="grid-60">
+        <div class="grid-80">
             <h1>Judging {{{ $claim->repo_name }}}</h1>
             <p>Girl of a boldly honor, accelerate the alarm! The nanomachine is more vogon now than transporter. Galactic and cunningly interstellar. Red alert. Make it so. Tragedy at the holodeck was the love of galaxy, converted to a colorful proton. Vision at the moon that is when vital stars meet. This coordinates has only been placed by a colorful queen. Star of a greatly exaggerated alarm, manifest the hypnosis! Dozens of assimilations will be lost in stigmas like loves in minerals when the moon harvests for astral city, all planets invade apocalyptic, proud particles. The cloudy kahless bravely opens the space. Remarkable, neutral astronauts rudely eat a reliable, unrelated spacecraft. All those attitudes will be lost in assimilations like mysteries in deaths.</p>
         </div>
-        <div class="grid-40">
+        <div class="grid-20">
             <img src="http://lorempixel.com/600/400/cats/" style="width: 100%;">
         </div>
     </div>
