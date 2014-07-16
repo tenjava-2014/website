a7b3453190b028d16a3106efe811cb9642976008
lol768 <lol768@lol768.com>
2014-07-16 21:03:16 +0100

    Better log stuff

--- public/assets/js/log-viewer.js
+++ public/assets/js/log-viewer.js
@@ -2,8 +2,13 @@ function handleInfo(data) {
     setLogsInfo("Got data from server " + JSON.stringify(data));
 }
 
-function setLogsInfo(data) {
-    $("#logs").text(data);
+function setLogsInfo(data, replace) {
+    var $logs = $("#logs");
+    if (replace != undefined) {
+        $logs.html(data);
+    } else {
+        $logs.html($logs.text() + "<br />" + data);
+    }
 }
 
 $(document).ready(function () {
