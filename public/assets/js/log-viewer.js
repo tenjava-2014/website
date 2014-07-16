614f3cec2c1718adf576bfb9ede41daa0cf77822
lol768 <lol768@lol768.com>
2014-07-16 20:58:47 +0100

    Specify a dataType

--- public/assets/js/log-viewer.js
+++ public/assets/js/log-viewer.js
@@ -1,11 +1,12 @@
 function handleInfo(data) {
-    alert(data);
+    alert(JSON.stringify(data));
 }
 
 $(document).ready(function () {
     var infoUrl = "/judging/logs/ajax";
     $.ajax(infoUrl, {
         type: 'GET',
+        dataType: 'json',
         success: handleInfo
     });
 });
