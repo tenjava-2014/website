e3868244b0f8bf79a1b13d85922181ad13be1852
lol768 <lol768@lol768.com>
2014-07-16 20:42:52 +0100

    Add additional data

--- app/TenJava/Controllers/Judging/LogViewController.php
+++ app/TenJava/Controllers/Judging/LogViewController.php
@@ -26,7 +26,7 @@ class LogViewController extends BaseJudgingController {
 
     public function testHmac() {
         $judgeId = $this->auth->getJudgeId();
-        $data = ["judge_id" => $judgeId];
+        $data = ["judge_id" => $judgeId, "timestamp" => time()];
         $dataSig = $this->hmac->createSignature(json_encode($data), $_ENV['API_TOKEN']);
         return Response::json($data, 200, ["X-Signature" => $dataSig]);
     }
