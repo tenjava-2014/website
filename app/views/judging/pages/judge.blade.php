b2d5b0b6a6c3ce064a2938ba157b9b772f913881
lol768 <lol768@lol768.com>
2014-07-16 19:32:23 +0100

    Add Idea category

--- app/views/judging/pages/judge.blade.php
+++ app/views/judging/pages/judge.blade.php
@@ -4,6 +4,9 @@
     <div class="grid-container">
         <div class="grid-80">
             <h1>Judging {{{ $claim->repo_name }}}</h1>
+            <p>Please judge plugins as objectively as possible.
+                You should be referring to our internal judging document while reviewing this submission.
+                If you think the entry needs to be disqualified or you have other concerns, please request oversight.</p>
 
             @if ($errors->any())
             <div class="alert block error">
@@ -18,18 +21,52 @@
 
             {{ Form::open(array('class' => 'form')) }}
             <input type="hidden" name="claim_id" value="{{{ $claim->id }}}">
+            <!--!-------------------!-->
+            <legend>Idea (75 points)</legend>
             <div class="control-group">
-                <label for="liked_phrase">Liked phrase</label>
+                <label for="idea_originality">Originality (15 points)</label>
                 <div class="control">
-                    {{ Form::text('liked_phrase', null, ['id' => 'liked_phrase', 'placeholder' => Lang::get("judging.input-helps.liked")]) }}
+                    {{ Form::text('idea_originality', null, ['id' => 'idea_originality']) }}
                 </div>
             </div>
             <div class="control-group">
-                <label for="comment">Improvement phrase</label>
+                <label for="idea_theme_conformance">Theme conformance (30 points)</label>
                 <div class="control">
-                    {{ Form::text('liked_phrase', null, ['id' => 'liked_phrase', 'placeholder' => Lang::get("judging.input-helps.improve")]) }}
+                    {{ Form::text('idea_theme_conformance', null, ['id' => 'idea_theme_conformance']) }}
                 </div>
             </div>
+            <div class="control-group">
+                <label for="idea_complexity">Complexity (10 points)</label>
+                <div class="control">
+                    {{ Form::text('idea_complexity', null, ['id' => 'idea_complexity']) }}
+                </div>
+            </div>
+            <div class="control-group">
+                <label for="idea_fun">Fun/usefulness (10 points)</label>
+                <div class="control">
+                    {{ Form::text('idea_fun', null, ['id' => 'idea_fun']) }}
+                </div>
+            </div>
+            <div class="control-group">
+                <label for="idea_expansion">Expansion potential (10 points)</label>
+                <div class="control">
+                    {{ Form::text('idea_expansion', null, ['id' => 'idea_expansion']) }}
+                </div>
+            </div>
+            <!--!-------------------!-->
+            <div class="control-group">
+                <label for="liked">Liked phrase</label>
+                <div class="control">
+                    {{ Form::text('liked', null, ['id' => 'liked_phrase', 'placeholder' => Lang::get("judging.input-helps.liked")]) }}
+                </div>
+            </div>
+            <div class="control-group">
+                <label for="improve">Improvement phrase</label>
+                <div class="control">
+                    {{ Form::text('improve', null, ['id' => 'improve', 'placeholder' => Lang::get("judging.input-helps.improve")]) }}
+                </div>
+            </div>
+            <!--!-------------------!-->
             <input type="submit" value="Send" class="button button-block button-flat-primary">
             {{ Form::close() }}
 
