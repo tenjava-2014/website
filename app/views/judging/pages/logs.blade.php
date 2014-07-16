831163c2154274b17a7c3b3046d1ef1fe425cdb5
lol768 <lol768@lol768.com>
2014-07-16 20:55:41 +0100

    Begin work on log viewer

--- app/views/judging/pages/logs.blade.php
+++ app/views/judging/pages/logs.blade.php
@@ -5,7 +5,11 @@
         <div class="grid-100">
             <h1>Log viewer</h1>
             <p>This page allows you to view a realtime feed of your test server's log file.</p>
+            <p></p>
         </div>
     </div>
 </div>
+@stop
+@section('post-scripts')
+<script type="application/javascript" src="{{ asset('/assets/js/log-viewer.js') }}"></script>
 @stop
---
+++ public/assets/js/log-viewer.js
@@ -0,0 +1,11 @@
+function handleInfo(data) {
+    alert(data);
+}
+
+$(document).ready(function () {
+    var infoUrl = "?judging/logs/ajax";
+    $.ajax(infoUrl, {
+        type: 'GET',
+        success: handleInfo
+    });
+});
