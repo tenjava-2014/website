4066924e6459dc14bca4eabcfd99ad39812712e3
lol768 <lol768@lol768.com>
2014-07-17 19:13:50 +0100

    Add log-viewer class

--- app/views/judging/pages/logs.blade.php
+++ app/views/judging/pages/logs.blade.php
@@ -5,7 +5,7 @@
         <div class="grid-100">
             <h1>Log viewer</h1>
             <p>This page allows you to view a realtime feed of your test server's log file.</p>
-            <div class="server-details">
+            <div class="server-details log-viewer">
             <pre id="logs">Log viewer app v0.1\n--------------------\nChecking for JS...</pre>
             </div>
         </div>
