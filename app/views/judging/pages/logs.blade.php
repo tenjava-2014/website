664fc797dcd4f0f3506a726246be6adbd13e4785
lol768 <lol768@users.noreply.github.com>
2014-07-26 15:31:10 +0100

    Remove redunant info

--- app/views/judging/pages/logs.blade.php
+++ app/views/judging/pages/logs.blade.php
@@ -4,7 +4,7 @@
     <div class="grid-container">
         <div class="grid-100">
             <h1>Log viewer</h1>
-            <p>This page allows you to view a realtime feed of your test server's log file. See a blank space? Your browser is blocking mixed content loading. Please ensure your traffic is not subject to an ongoing MiTM attack, enable mixed content loading and disable once you are finished with the log viewer.</p>
+            <p>This page allows you to view a realtime feed of your test server's log file.</p>
             <div class="server-details log-viewer">
             <pre id="logs">Log viewer app v0.1\n--------------------\nChecking for JS...</pre>
             </div>
