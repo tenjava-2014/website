93e1db9fccf31039a07bdc6d93ef29e765fe67a5
lol768 <lol768@lol768.com>
2014-07-31 21:48:37 +0100

    Green bars for awesome judges

--- app/views/pages/dynamic/judging_stats.blade.php
+++ app/views/pages/dynamic/judging_stats.blade.php
@@ -32,10 +32,11 @@
                     $per = (floatval($i) / $totalAssigned) * 100;
                     $per = (int) $per;
                 }
+                $finished = ($totalAssigned === $i);
                 ?>
                 <!-- forgive me -->
                 <div class="judge-progress" style="width: 100%; background-color: #aaa; padding: 0;">
-                    <div style="margin: 0; background-color: #3590E6; width: {{{ $per }}}%; padding: 0;">
+                    <div style="margin: 0; background-color: {{ $finished ? "#7DB500" : "#3590E6" }}; width: {{{ $per }}}%; padding: 0;">
                         <h3 style="white-space: nowrap; margin: 0; padding: 5px; color: #fff;">{{{ $judge->github_name }}} ({{{ $i }}}/{{{ $totalAssigned }}})</h3></div>
                 </div>
             @endforeach
