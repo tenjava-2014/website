4f2e359890928481ea8aead05a9bdf5bc450d92e
lol768 <lol768@lol768.com>
2014-08-10 17:38:15 +0100

    Fix array access

--- app/views/pages/dynamic/judging_stats.blade.php
+++ app/views/pages/dynamic/judging_stats.blade.php
@@ -19,8 +19,8 @@
             @foreach ($judges as $judge)
                 <!-- forgive me -->
                 <div class="judge-progress" style="width: 100%; background-color: #aaa; padding: 0;">
-                    <div style="margin: 0; background-color: {{ $finished ? "#7DB500" : "#3590E6" }}; width: {{{ $per }}}%; padding: 0;">
-                        <h3 style="white-space: nowrap; margin: 0; padding: 5px; color: #fff;">{{{ $judge->github_name }}} ({{{ $judge_progress[$judge->github_name] }}}/{{{ $totalAssigned }}})</h3>
+                    <div style="margin: 0; background-color: {{ $judge_progress[$judge->github_name]['finished'] ? "#7DB500" : "#3590E6" }}; width: {{{ $per }}}%; padding: 0;">
+                        <h3 style="white-space: nowrap; margin: 0; padding: 5px; color: #fff;">{{{ $judge->github_name }}} ({{{ $judge_progress[$judge->github_name]['assigned'] }}}/{{{ $judge_progress[$judge->github_name]['completed'] }}})</h3>
                     </div>
                 </div>
             @endforeach
