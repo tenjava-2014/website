f42e7c0284316645ece4132b7464794d4f54292b
lol768 <lol768@lol768.com>
2014-08-24 15:03:13 +0100

    Begin work on graphical results viewer for organizers

---
+++ app/TenJava/Controllers/Judging/ReviewController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace TenJava\Controllers\Judging;
+
+use TenJava\Controllers\Abstracts\BaseJudgingController;
+
+class ReviewController extends BaseJudgingController {
+
+    public function displayResultsForParticipant($repoName) {
+        // We'll be given a repo name such as lol768-t1 here.
+        die("Access!");
+    }
+}
---
+++ app/views/judging/pages/result-view.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.judging')
+@section('content')
+<div class="content-back">
+    <div class="grid-container">
+        <div class="grid-100">
+            <h1>Administrator results viewer</h1>
+            <p>This page is designed to be used by organizers in order to quickly review the assigned scores and notes given for a specific participant.
+            This will be of use during the review process.</p>
+
+            <div class="alert basic error">This page is restricted.
+                <p>This data should not be disclosed to judges, participants or the public under any circumstances.</p>
+            </div>
+
+        </div>
+    </div>
+</div>
+@stop
