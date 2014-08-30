9000460157bbd414b0f51ff85167fea8eb8f0987
lol768 <lol768@lol768.com>
2014-08-30 19:02:13 +0100

    Clarify when we're gonna announce the results

--- app/views/pages/secure/results-unavailable.blade.php
+++ app/views/pages/secure/results-unavailable.blade.php
@@ -6,7 +6,7 @@
         <p>
             We haven't published the results yet. We'll be starting the
             <a href="https://www.youtube.com/watch?v=cQ-FQ9ee2Yo">live stream</a> for the results at 19:00 UTC on
-            August 30th. This page will be updated once we've announced the results via the stream.
+            August 30th. This page will be updated after the results event is over with entry descriptions etc.
         </p>
 	</div>
 </div>
