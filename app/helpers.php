e03f94a9f4336badb97cc73c9091588b73360bc7
lol768 <lol768@lol768.com>
2014-09-01 22:43:30 +0100

    Hahaha, that serves me right for not reading the code of functions I copy :D

--- app/helpers.php
+++ app/helpers.php
@@ -7,5 +7,5 @@
 function ordinal($number) {
     $ends = array('th','st','nd','rd','th','th','th','th','th','th');
     $mod100 = $number % 100;
-    return $number . ($mod100 >= 11 && $mod100 <= 13 ? 'th' : $ends[$number % 10]);
+    return ($mod100 >= 11 && $mod100 <= 13 ? 'th' : $ends[$number % 10]);
 }
