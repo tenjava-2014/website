896bcfd8ff6838331e79e1dfb4ac7fd62d67a26f
Kyle Clemens <jkc.clemens@gmail.com>
2014-09-21 15:19:47 -0400

    Fixed: Use HTML

--- app/views/emails/news/test.blade.php
+++ app/views/emails/news/test.blade.php
@@ -1,11 +1,17 @@
-Hey, {{{ $name }}}!
-
-This is a test email verifying your subscription to ten.java updates. This should never be sent to anyone but jkcclemens, really.
-
-Here's some data!
-Name: {{{ $name }}}
-Email: {{{ $email }}}
-ID: {{{ $id }}}
-
-Thanks,
-The ten.java Team
+<h1>Hey, {{{ $name }}}!</h1>
+<p>
+    This is a test email verifying your subscription to ten.java updates. This should never be sent to anyone but
+    jkcclemens, really.
+</p>
+<p>
+    Here's some data!
+</p>
+<ul>
+    <li>Name: {{{ $name }}}</li>
+    <li>Email: {{{ $email }}}</li>
+    <li>ID: {{{ $id }}}</li>
+</ul>
+<p>
+    Thanks,<br/>
+    The ten.java Team
+</p>
