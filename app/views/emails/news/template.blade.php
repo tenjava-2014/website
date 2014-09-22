ae45f6badf83efe41120057715f62d9f3e9ad0b7
Kyle Clemens <jkc.clemens@gmail.com>
2014-09-21 20:50:48 -0400

    Updated: Template fixes

--- app/views/emails/news/template.blade.php
+++ app/views/emails/news/template.blade.php
@@ -1,18 +1,24 @@
-<style>
-    .footer {
-        font-size: .75em;
-        color: #999;
-    }
-    a {
-        text-decoration: none;
-        color: #3b8bba;
-    }
-    a:focus, a:hover {
-        text-decoration: underline;
-    }
-</style>
+<!doctype html>
+<html lang="en">
+<head>
+    <style>
+        .footer {
+            font-size: .75em;
+            color: #999;
+        }
+        a {
+            text-decoration: none;
+            color: #3b8bba;
+        }
+        a:focus, a:hover {
+            text-decoration: underline;
+        }
+    </style>
+</head>
+<body>
 @yield('content')
-
 <p class="footer">
     Don't want to receive these emails anymore? <a href="{{{ $unsubscribe_url }}}">Unsubscribe</a>.
 </p>
+</body>
+</html>
