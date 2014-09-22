7d84898cb159488a796e87eae4a0c18f4d265fd6
Kyle Clemens <jkc.clemens@gmail.com>
2014-09-21 20:56:33 -0400

    Updated: More template work

--- app/views/emails/news/template.blade.php
+++ app/views/emails/news/template.blade.php
@@ -2,6 +2,10 @@
 <html lang="en">
 <head>
     <style>
+        body {
+            font-family: sans-serif;
+            color: #333;
+        }
         .footer {
             font-size: .75em;
             color: #999;
@@ -9,9 +13,6 @@
         a {
             text-decoration: none;
             color: #3b8bba;
-        }
-        a:focus, a:hover {
-            text-decoration: underline;
         }
     </style>
 </head>
