8b0a35b755e2deea674eaefb40cb916c821959d4
lol768 <lol768@lol768.com>
2014-09-23 16:37:25 +0100

    Stop ABP being silly

--- app/views/partials/footer.blade.php
+++ app/views/partials/footer.blade.php
@@ -10,7 +10,7 @@
 			<ul class="soc-links">
 				<li><a href="mailto:{{{ HTML::obfuscate('tenjava@' . 'ten' . 'java.com') }}}"><i class="fa fa-2x fa-envelope-square"></i></a></li>
 				<li><a title="" target="_blank" href="https://github.com/tenjava"><i class="fa fa-2x fa-github-square"></i></a></li>
-				<li><a target="_blank" href="https://twitter.com/tenjava"><i class="fa fa-2x fa-twitter-square"></i></a></li>
+				<li><a target="_blank" href="https://twitter.com/tenjava"><i class="fa fa-2x fa-generic-bird-square"></i></a></li>
 				<li><a target="_blank" href="http://forums.bukkit.org/threads/269253/"><span class="fa-stack bottom"><i class="fa fa-square fa-stack-2x"></i><i class="fa fa-comment fa-stack-1x black"></i></span></a></li>
 			</ul>
 		</div>
