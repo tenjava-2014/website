6cc3f6920ebbe65770be66bdc22278c57845ffc2
Kyle Clemens <jkc.clemens@gmail.com>
2014-09-27 16:21:52 -0400

    Added: Teams email

---
+++ app/views/emails/news/teams.blade.php
@@ -0,0 +1,40 @@
+@extends('emails.news.template')
+@section('content')
+    <h1>Hey, {{{ $name }}}!</h1>
+    <p>
+        It's official. The next ten.java is on its way, and it comes bearing some big news.
+    </p>
+    <h2>What?</h2>
+    <p>
+        New to ten.java? That's okay! ten.java is a biannual competition in which participants create plugins for
+        Minecraft based off of a theme. The first two competitions have been ten, consecutive hours long. This is an
+        announcement for the next, upcoming competition.
+    </p>
+    <h2>When?</h2>
+    <p>
+        Hopefully sometime this winter. We are currently planning for December. The when all depends on what happens
+        when the dust settles around Minecraft.
+    </p>
+    <h2>How?</h2>
+    <p>
+        In teams! It's been suggested a ton, and we think that it's a great idea. The next ten.java will be a team-based
+        competition, in which a group of people work together to create an awesome mod/plugin based on a theme.
+    </p>
+    <p>
+        The current plan is to have teams be created on the website (tenjava.com) by a team leader. Then, members can be
+        invited by the leader to join the team. Teams will have a maximum of five members and a minimum of three members
+        (leader included in both counts). We will also have a method for participants to meet other participants without
+        a team in order to form a new team, so no one is left out. Remember: none of this can be done yet. This is just
+        an announcement of our plans.
+    </p>
+    <p>
+        That's all that there is to officially announce currently. There are definitely more things in planning! When
+        there is more that is officially decided upon, another topic will be made.
+    </p>
+    <p>
+        Definitely looking forward to the next one. It's gonna be great.
+    </p>
+    <small>
+        This was originally posted <a href="http://forums.tenjava.com/t/the-next-ten-java-teams-teams-everywhere/119">here</a>.
+    </small>
+@stop
