6380c28f9a46fe92e95d60d27ddcf8ce8ff6ed0c
Kyle Clemens <jkc.clemens@gmail.com>
2014-10-10 01:02:50 -0400

    Fixed: Silly indentation

--- resources/views/layouts/judging.blade.php
+++ resources/views/layouts/judging.blade.php
@@ -1,8 +1,8 @@
 <!doctype html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8" />
-	<title>{{ $titleAdd or "ten.java" }}</title>
+    <meta charset="UTF-8"/>
+    <title>{{ $titleAdd or "ten.java" }}</title>
     <link rel="shortcut icon" href="{!! asset('/assets/img/icons/favicon.ico') !!}" type="image/x-icon"/>
     <link rel="apple-touch-icon" href="{!! asset('/assets/img/icons/apple-touch-icon.png') !!}"/>
     <link rel="apple-touch-icon" sizes="57x57" href="{!! asset('/assets/img/icons/apple-touch-icon-57x57.png') !!}"/>
@@ -12,28 +12,28 @@
     <link rel="apple-touch-icon" sizes="120x120" href="{!! asset('/assets/img/icons/apple-touch-icon-120x120.png') !!}"/>
     <link rel="apple-touch-icon" sizes="144x144" href="{!! asset('/assets/img/icons/apple-touch-icon-144x144.png') !!}"/>
     <link rel="apple-touch-icon" sizes="152x152" href="{!! asset('/assets/img/icons/apple-touch-icon-152x152.png') !!}"/>
-	<link href="{!! asset('/assets/css/grid.css') !!}" rel="stylesheet" />
-	<link href="{!! asset('/assets/css/styles.css') !!}?v=1.51" rel="stylesheet" />
+    <link href="{!! asset('/assets/css/grid.css') !!}" rel="stylesheet"/>
+    <link href="{!! asset('/assets/css/styles.css') !!}?v=1.51" rel="stylesheet"/>
     <script type="application/javascript" src="//cdnjs.cloudflare.com/ajax/libs/fastclick/1.0.0/fastclick.min.js"></script>
     @yield('additional-scripts')
-	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.1.0/css/font-awesome.min.css">
-	<meta name="description" content="Ten hour Bukkit plugin development contest." />
-	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1" />
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.1.0/css/font-awesome.min.css">
+    <meta name="description" content="Ten hour Bukkit plugin development contest."/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1"/>
 </head>
 <body class="judge-interface">
 <div id="wrapper">
     @include('partials.nav', ['logo' => asset('/assets/img/logo_light_judge.svg')])
-	<div id="point-ticker">
-		<div class="grid-container">
-			<div class="grid-20 tablet-grid-20">Actual participants: <span>{{ $turnout['real'] }}</span></div>
-			<div class="grid-20 tablet-grid-20">Turnout: <span>{{ sprintf("%.2f%%", ($turnout['real'] / ($turnout['total'] > 0 ? $turnout['total'] : 1)) * 100) }}</span></div>
-			<div class="grid-20 tablet-grid-20">Assigned entries: <span>{{ $claims['total'] }}</span></div>
-			<div class="grid-20 tablet-grid-20">Completed entries: <span>{{ count($claims['done']) }}</span></div>
-			<div class="grid-20 tablet-grid-20">Entries remaining: <span>{{ count($claims['pending']) }}</span></div>
-		</div>
-	</div>
-	@yield('content')
-	<div class="push"></div>
+    <div id="point-ticker">
+        <div class="grid-container">
+            <div class="grid-20 tablet-grid-20">Actual participants: <span>{{ $turnout['real'] }}</span></div>
+            <div class="grid-20 tablet-grid-20">Turnout: <span>{{ sprintf("%.2f%%", ($turnout['real'] / ($turnout['total'] > 0 ? $turnout['total'] : 1)) * 100) }}</span></div>
+            <div class="grid-20 tablet-grid-20">Assigned entries: <span>{{ $claims['total'] }}</span></div>
+            <div class="grid-20 tablet-grid-20">Completed entries: <span>{{ count($claims['done']) }}</span></div>
+            <div class="grid-20 tablet-grid-20">Entries remaining: <span>{{ count($claims['pending']) }}</span></div>
+        </div>
+    </div>
+    @yield('content')
+    <div class="push"></div>
 </div>
 @include('partials.footer')
 <script type="application/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,8 +1,8 @@
 <!doctype html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8"/>
-	<title>{{ $titleAdd or "ten.java" }}</title>
+    <meta charset="UTF-8"/>
+    <title>{{ $titleAdd or "ten.java" }}</title>
     <link rel="shortcut icon" href="{!! asset('/assets/img/icons/favicon.ico') !!}" type="image/x-icon"/>
     <link rel="apple-touch-icon" href="{!! asset('/assets/img/icons/apple-touch-icon.png') !!}"/>
     <link rel="apple-touch-icon" sizes="57x57" href="{!! asset('/assets/img/icons/apple-touch-icon-57x57.png') !!}"/>
@@ -12,26 +12,26 @@
     <link rel="apple-touch-icon" sizes="120x120" href="{!! asset('/assets/img/icons/apple-touch-icon-120x120.png') !!}"/>
     <link rel="apple-touch-icon" sizes="144x144" href="{!! asset('/assets/img/icons/apple-touch-icon-144x144.png') !!}"/>
     <link rel="apple-touch-icon" sizes="152x152" href="{!! asset('/assets/img/icons/apple-touch-icon-152x152.png') !!}"/>
-	<link href="{!! asset('/assets/css/grid.css') !!}" rel="stylesheet"/>
-	<link href="{!! asset('/assets/css/styles.css') !!}?v=1.53" rel="stylesheet"/>
+    <link href="{!! asset('/assets/css/grid.css') !!}" rel="stylesheet"/>
+    <link href="{!! asset('/assets/css/styles.css') !!}?v=1.53" rel="stylesheet"/>
     @yield('additional-scripts')
-	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.1.0/css/font-awesome.min.css">
-	<meta name="description" content="Ten hour Bukkit plugin development contest."/>
-	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1"/>
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.1.0/css/font-awesome.min.css">
+    <meta name="description" content="Ten hour Bukkit plugin development contest."/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1"/>
 </head>
 <body>
 <div id="wrapper">
-	@include('partials.nav', ['logo' => asset(app()->env == 'prod' ? '/assets/img/logo_light.svg' : '/assets/img/logo_light_beta.svg')])
-	<div id="point-ticker">
-		<div class="grid-container">
+    @include('partials.nav', ['logo' => asset(app()->env == 'prod' ? '/assets/img/logo_light.svg' : '/assets/img/logo_light_beta.svg')])
+    <div id="point-ticker">
+        <div class="grid-container">
             <div class="grid-25 tablet-grid-20"></div>
             <div class="grid-25 tablet-grid-20"></div>
             <div class="grid-25 tablet-grid-20"></div>
             <div class="grid-25 tablet-grid-20"></div>
-		</div>
-	</div>
-	@yield('content')
-	<div class="push"></div>
+        </div>
+    </div>
+    @yield('content')
+    <div class="push"></div>
 </div>
 @include('partials.footer')
 <script type="application/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
