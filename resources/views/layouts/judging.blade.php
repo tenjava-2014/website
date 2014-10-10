fe7081507c007297c127e2c99b7244247d9177ba
Kyle Clemens <jkc.clemens@gmail.com>
2014-10-10 00:59:43 -0400

    Updated: More icons!

--- resources/views/layouts/judging.blade.php
+++ resources/views/layouts/judging.blade.php
@@ -3,7 +3,15 @@
 <head>
 	<meta charset="UTF-8" />
 	<title>{{ $titleAdd or "ten.java" }}</title>
-	<link rel="icon" type="image/png" href="{!! asset('/assets/img/favicon.ico') !!}" />
+    <link rel="shortcut icon" href="{!! asset('/assets/img/icons/favicon.ico') !!}" type="image/x-icon"/>
+    <link rel="apple-touch-icon" href="{!! asset('/assets/img/icons/apple-touch-icon.png') !!}"/>
+    <link rel="apple-touch-icon" sizes="57x57" href="{!! asset('/assets/img/icons/apple-touch-icon-57x57.png') !!}"/>
+    <link rel="apple-touch-icon" sizes="72x72" href="{!! asset('/assets/img/icons/apple-touch-icon-72x72.png') !!}"/>
+    <link rel="apple-touch-icon" sizes="76x76" href="{!! asset('/assets/img/icons/apple-touch-icon-76x76.png') !!}"/>
+    <link rel="apple-touch-icon" sizes="114x114" href="{!! asset('/assets/img/icons/apple-touch-icon-114x114.png') !!}"/>
+    <link rel="apple-touch-icon" sizes="120x120" href="{!! asset('/assets/img/icons/apple-touch-icon-120x120.png') !!}"/>
+    <link rel="apple-touch-icon" sizes="144x144" href="{!! asset('/assets/img/icons/apple-touch-icon-144x144.png') !!}"/>
+    <link rel="apple-touch-icon" sizes="152x152" href="{!! asset('/assets/img/icons/apple-touch-icon-152x152.png') !!}"/>
 	<link href="{!! asset('/assets/css/grid.css') !!}" rel="stylesheet" />
 	<link href="{!! asset('/assets/css/styles.css') !!}?v=1.51" rel="stylesheet" />
     <script type="application/javascript" src="//cdnjs.cloudflare.com/ajax/libs/fastclick/1.0.0/fastclick.min.js"></script>
