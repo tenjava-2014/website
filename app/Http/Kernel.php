623446c6c54529675cd33a80e95d7853bec5eace
lol768 <lol768@lol768.com>
2014-11-27 21:38:20 +0000

    Fix up a bunch of stuff.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -5,35 +5,29 @@ use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel {
 
-    /**
-     * The application's HTTP middleware stack.
-     *
-     * @var array
-     */
-    protected $middleware = [
-        'TenJava\Http\Middleware\UnderMaintenance',
-        'Illuminate\Cookie\Middleware\EncryptCookies',
-        'Illuminate\Cookie\Middleware\AddQueuedCookiesToRequest',
-        'Illuminate\Session\Middleware\ReadSession',
-        'Illuminate\Session\Middleware\WriteSession',
-        'Illuminate\View\Middleware\ShareErrorsFromSession',
-        'TenJava\Http\Middleware\VerifyCsrfToken',
-    ];
+	/**
+	 * The application's global HTTP middleware stack.
+	 *
+	 * @var array
+	 */
+	protected $middleware = [
+		'Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode',
+		'Illuminate\Cookie\Middleware\EncryptCookies',
+		'Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse',
+		'Illuminate\Session\Middleware\StartSession',
+		'Illuminate\View\Middleware\ShareErrorsFromSession',
+		'Illuminate\Foundation\Http\Middleware\VerifyCsrfToken',
+	];
 
-    /**
-     * Handle an incoming HTTP request.
-     *
-     * @param  \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
-     */
-    public function handle($request) {
-        try {
-            return parent::handle($request);
-        } catch (Exception $e) {
-            $this->reportException($e);
-
-            return $this->renderException($request, $e);
-        }
-    }
+	/**
+	 * The application's route middleware.
+	 *
+	 * @var array
+	 */
+	protected $routeMiddleware = [
+		'auth' => 'App\Http\Middleware\Authenticate',
+		'auth.basic' => 'Illuminate\Auth\Middleware\AuthenticateWithBasicAuth',
+		'guest' => 'App\Http\Middleware\RedirectIfAuthenticated',
+	];
 
 }
